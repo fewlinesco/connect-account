dd3630e18ab7f1d37d73a68ab92b85fec3565470
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-06-23 14:44:13 +0200

    type def + some cleaning

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import React from "react";
+
+const Index: NextPage = () => (
+  <>
+    <Head>
+      <title>Sparta Account</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <main>
+      <div>
+        <h1>Welcome to Connect Account</h1>
+      </div>
+    </main>
+  </>
+);
+
+export default Index;
