1514b292fac3e0328f559899ca13604de90357f9
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-07-17 10:45:09 +0200

    added jest + getServerSideProps test

---
+++ tests/serverSideProps.test.ts
@@ -0,0 +1,52 @@
+import { IncomingMessage, ServerResponse } from "http";
+import fetch from "jest-fetch-mock";
+import { Socket } from "net";
+
+import { getServerSideProps } from "../src/pages/index";
+
+fetch.enableMocks();
+
+describe("getServerSideProps", () => {
+  beforeEach(() => {
+    fetch.resetMocks();
+  });
+
+  const mockedContext = {
+    req: new IncomingMessage(new Socket()),
+    res: new ServerResponse(new IncomingMessage(new Socket())),
+    query: { foo: "bar" },
+  };
+
+  it("should handle errors", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ status: "error" }));
+
+    const response = await getServerSideProps(mockedContext);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          initialDog: {
+            status: "error",
+          },
+        },
+      }),
+    );
+  });
+
+  it("should call api", async () => {
+    fetch.mockResponseOnce(JSON.stringify({ status: "success", data: "foo" }));
+
+    const response = await getServerSideProps(mockedContext);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          initialDog: {
+            status: "success",
+            data: "foo",
+          },
+        },
+      }),
+    );
+  });
+});
