04eca23fafc7d33b8c039a467ab0b83006969f93
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-07-20 11:07:25 +0200

    Finalised test

--- tests/serverSideProps.test.ts
+++ tests/serverSideProps.test.ts
@@ -1,10 +1,11 @@
 import { IncomingMessage, ServerResponse } from "http";
 import fetch from "jest-fetch-mock";
+import { enableFetchMocks } from "jest-fetch-mock";
 import { Socket } from "net";
 
 import { getServerSideProps } from "../src/pages/index";
 
-fetch.enableMocks();
+enableFetchMocks();
 
 describe("getServerSideProps", () => {
   beforeEach(() => {
@@ -17,34 +18,41 @@ describe("getServerSideProps", () => {
     query: {},
   };
 
-  it("should handle errors", async () => {
-    fetch.mockResponseOnce(JSON.stringify({ status: "error" }));
-
-    const response = await getServerSideProps(mockedContext);
-
-    expect(response).toEqual(
-      expect.objectContaining({
-        props: {
-          fetchedData: {
-            status: "error",
-          },
+  const mockedResponse = {
+    data: {
+      provider: {
+        id: "4a5f8589-0d91-4a69-924a-6f227a69666d",
+        name: "Mocked Provider",
+        user: {
+          id: "299d268e-3e19-4486-9be7-29c539d241ac",
+          identities: [
+            {
+              primary: true,
+              status: "validated",
+              type: "phone",
+              value: "0123456789",
+            },
+            {
+              primary: true,
+              status: "validated",
+              type: "email",
+              value: "test@test.test",
+            },
+          ],
         },
-      }),
-    );
-  });
+      },
+    },
+  };
 
   it("should call api", async () => {
-    fetch.mockResponseOnce(JSON.stringify({ status: "success", data: "foo" }));
+    fetch.once(JSON.stringify(mockedResponse));
 
     const response = await getServerSideProps(mockedContext);
 
     expect(response).toEqual(
       expect.objectContaining({
         props: {
-          fetchedData: {
-            status: "success",
-            data: "foo",
-          },
+          fetchedData: mockedResponse,
         },
       }),
     );
