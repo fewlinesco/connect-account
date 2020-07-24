eafe5cb627a0255abd09b74bece400401f2c1537
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-07-24 10:34:36 +0200

    Migrated the console error filter to jest setup file

--- tests/addEmailIdentityWorkflow.test.tsx
+++ tests/addEmailIdentityWorkflow.test.tsx
@@ -6,40 +6,6 @@ import { ProviderUser } from "../src/@types/ProviderUser";
 import Index from "../src/pages/index";
 import { render, fireEvent } from "./config/test-utils";
 
-// Following code mocks window.console.error
-// to ignore the "Not implemented: HTMLFormElement.prototype.submit".
-//
-// Problem: We use "form.onsubmit" event listener in some tests,
-// but HTMLFormElement.prototype.submit is not implemented in JSDOM,
-// although the tests are passing and handler fires.
-//
-// More:
-// https://github.com/jsdom/jsdom/issues/1937
-// https://github.com/facebook/jest/issues/5223#issuecomment-489422244
-
-let origErrorConsole: Console["error"];
-
-beforeEach(() => {
-  origErrorConsole = window.console.error;
-
-  window.console.error = (...args: string[]) => {
-    const firstArg = args.length > 0 && args[0];
-
-    const shouldBeIgnored =
-      firstArg &&
-      typeof firstArg === "string" &&
-      firstArg.includes("Not implemented: HTMLFormElement.prototype.submit");
-
-    if (!shouldBeIgnored) {
-      origErrorConsole(...args);
-    }
-  };
-});
-
-afterEach(() => {
-  window.console.error = origErrorConsole;
-});
-
 enableFetchMocks();
 
 describe("addEmailIdentityWorkflow", () => {
