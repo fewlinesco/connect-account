e24ca0e64b4261d63acee54b5898dec181619c72
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-07-27 13:08:17 +0200

    Refacto app wrapper to use it in testing

--- tests/config/test-utils.tsx
+++ tests/config/test-utils.tsx
@@ -1,27 +1,13 @@
 import { render, RenderResult } from "@testing-library/react";
-import { AppProps } from "next/app";
-import React from "react";
-import { ThemeProvider } from "styled-components";
 
-import { Layout } from "../../src/components/Layout";
-import { GlobalStyle } from "../../src/design-system/globals/globalStyle";
-import { lightTheme } from "../../src/design-system/theme/lightTheme";
-
-const AllTheProviders: React.FC<JSX.Element> = ({ children }) => {
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <Layout>{children}</Layout>
-    </ThemeProvider>
-  );
-};
+import { AccountApp } from "../../src/pages/_app";
 
 const customRender = (
   ui: any,
   options: any,
 ): RenderResult<
   typeof import("../../node_modules/@testing-library/dom/types/queries")
-> => render(ui, { wrapper: AllTheProviders, ...options });
+> => render(ui, { wrapper: AccountApp, ...options });
 
 // Re-export everything.
 export * from "@testing-library/react";
