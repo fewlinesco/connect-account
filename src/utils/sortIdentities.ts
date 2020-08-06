074d6a99ac85a9a2c0468a6cd06b68abb86577d3
Hamza Ali <hamza.ali@btwin.com>
2020-08-06 15:27:12 +0200

    using ReceivedIdentityTypes in sortIdentities

--- src/utils/sortIdentities.ts
+++ src/utils/sortIdentities.ts
@@ -1,6 +1,6 @@
 import { ProviderUser } from "src/@types/ProviderUser";
 
-import { IdentityTypes, Identity } from "../@types/Identity";
+import { ReceivedIdentityTypes, Identity } from "../@types/Identity";
 import { SortedIdentities } from "../@types/SortedIdentities";
 
 export function sortIdentities(fetchedData: {
@@ -11,9 +11,9 @@ export function sortIdentities(fetchedData: {
   if (fetchedData.data) {
     const identities: Identity[] = fetchedData.data.provider.user.identities;
     identities.forEach((identity) => {
-      if (identity.type === IdentityTypes.EMAIL.toLowerCase()) {
+      if (identity.type === ReceivedIdentityTypes.EMAIL) {
         emailIdentities.push(identity);
-      } else if (identity.type === IdentityTypes.PHONE.toLowerCase()) {
+      } else if (identity.type === ReceivedIdentityTypes.PHONE) {
         phoneIdentities.push(identity);
       }
     });
