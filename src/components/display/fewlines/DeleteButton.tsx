3a5cc68f671a9e45c58243f2a246dda6e163482f
Hamza Ali <hamza.ali@btwin.com>
2020-08-31 10:12:35 +0200

    making the delete button redirect to the homepage

--- src/components/display/fewlines/DeleteButton.tsx
+++ src/components/display/fewlines/DeleteButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 type DeleteButtonProps = {
-  deleteIdentity: () => Promise<Response>;
+  deleteIdentity: () => Promise<void>;
 };
 
 export const DeleteButton: React.FC<DeleteButtonProps> = ({
