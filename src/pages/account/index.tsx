373d72dae8e44474f8149ba4e55cd87953afb4b4
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-09-08 14:28:39 +0200

    PR amends

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -38,7 +38,6 @@ export const getServerSideProps: GetServerSideProps = withSSRLogger(
       const accessToken = context.req.session.get("user-jwt");
 
       if (accessToken) {
-        console.log(accessToken);
         await oauth2Client.verifyJWT<{ sub: string }>(
           accessToken,
           config.connectJwtAlgorithm,
