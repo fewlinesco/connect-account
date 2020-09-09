5b7bd6c174d2b708e3075477b55d1b55e9d36992
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-09-09 15:05:24 +0200

    checkVerificationCode command

---
+++ lib/@types/Identity.ts
@@ -0,0 +1,15 @@
+export enum IdentityTypes {
+  APPLE = "APPLE",
+  EMAIL = "EMAIL",
+  FACEBOOK = "FACEBOOK",
+  GITHUB = "GITHUB",
+  GOOGLE = "GOOGLE",
+  KAKAO_TALK = "KAKAO_TALK",
+  LINE = "LINE",
+  NAVER = "NAVER",
+  PAYPAL = "PAYPAL",
+  PHONE = "PHONE",
+  PROVIDER = "PROVIDER",
+  STRAVA = "STRAVA",
+  VKONTAKTE = "VKONTAKTE",
+}
---
+++ lib/checkVerificationCode.ts
@@ -0,0 +1,56 @@
+import { FetchResult } from "apollo-link";
+import gql from "graphql-tag";
+
+import { fetchManagement } from "../src/utils/fetchManagement";
+import { IdentityTypes } from "./@types/Identity";
+
+export type CheckVerificationCodeInput = {
+  code: string;
+  eventId: string;
+};
+
+enum CheckVerificationCodeStatus {
+  EXPIRED = "EXPIRED",
+  INVALID = "INVALID",
+  NOT_FOUND = "NOT_FOUND",
+  VALID = "VALID",
+}
+
+export type CheckVerificationCodeResult = {
+  identityType: IdentityTypes;
+  identityValue: string;
+  nonce: string;
+  status: CheckVerificationCodeStatus;
+};
+
+const CHECK_VERIFICATION_CODE = gql`
+  query checkVerificationCode(input: CheckVerificationCodeInput){
+    checkVerificationCode(input: $input) {
+      identityType
+      identityValue
+      nonce
+      status
+      __typename
+    }
+  }
+`;
+
+export async function checkVerificationCode(
+  command: CheckVerificationCodeInput,
+): Promise<
+  | FetchResult<{
+      sendIdentityValidationCode: CheckVerificationCodeResult;
+    }>
+  | Error
+> {
+  const operation = {
+    query: CHECK_VERIFICATION_CODE,
+    variables: command,
+  };
+
+  return (await fetchManagement(operation)) as
+    | FetchResult<{
+        sendIdentityValidationCode: CheckVerificationCodeResult;
+      }>
+    | Error;
+}
