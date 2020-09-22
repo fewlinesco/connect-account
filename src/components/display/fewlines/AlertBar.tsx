e1c037dee7ea44d9eba0d4e32b5571c096fe393d
Hamza Ali <hamza.ali@btwin.com>
2020-09-22 17:42:26 +0200

    creating a parent Wrapper

--- src/components/display/fewlines/AlertBar.tsx
+++ src/components/display/fewlines/AlertBar.tsx
@@ -5,28 +5,44 @@ import { IdentityTypes } from "../../../@types/Identity";
 
 export const AlertBar: React.FC<{ type: IdentityTypes }> = ({ type }) => {
   if (type.toUpperCase() === IdentityTypes.EMAIL) {
-    return <Alert> Confirmation email has been sent</Alert>;
+    return (
+      <Wrapper>
+        <Alert> Confirmation email has been sent</Alert>
+      </Wrapper>
+    );
   }
   if (type.toUpperCase() === IdentityTypes.PHONE) {
-    return <Alert>Confirmation SMS has been sent</Alert>;
+    return (
+      <Wrapper>
+        <Alert>Confirmation SMS has been sent</Alert>
+      </Wrapper>
+    );
   }
   return null;
 };
 
 export default AlertBar;
 
+const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const Alert = styled.div`
   position: fixed;
   min-width: 25rem;
   max-width: 90%;
-  margin-left: -12.5rem;
   background-color: ${({ theme }) => theme.colors.black};
   color: ${({ theme }) => theme.colors.background};
   border-radius: 0.4rem;
   text-align: center;
   padding: 1.6rem;
   z-index: 1;
-  left: 50%;
   top: 2rem;
   visibility: hidden;
   opacity: 0;
@@ -39,8 +55,8 @@ const Alert = styled.div`
       visibility: hidden;
     }
 
-    20%,
-    80% {
+    10%,
+    90% {
       opacity: 1;
       visibility: visible;
     }
