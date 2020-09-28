ec90d78c9afbad80a1e161c0de5dd78a9c4e8d05
Hamza <33835824+Hamza-A@users.noreply.github.com>
2020-09-28 15:39:57 +0200

    Cu 8gw1gx creating containers for stories (#43)
    Co-authored-by: Hamza Ali <hamza.ali@btwin.com>

--- src/components/display/fewlines/IdentityContainer/IdentityContainer.stories.tsx
+++ src/components/display/fewlines/IdentityContainer/IdentityContainer.stories.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
 import { IdentityContainer } from "./IdentityContainer";
 
 export default { title: "IdentityContainer", component: IdentityContainer };
 
 export const StandardIdentityContainer = (): JSX.Element => {
-  return <IdentityContainer>emailaddress@mail.test</IdentityContainer>;
+  return (
+    <Container>
+      <IdentityContainer>emailaddress@mail.test</IdentityContainer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`;
