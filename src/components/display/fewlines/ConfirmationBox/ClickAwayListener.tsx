a5a85c96216f57e48ae75d8894e6372f950a7e62
Hamza Ali <hamza.ali@btwin.com>
2020-10-06 12:57:54 +0200

    changing click away listener css

--- src/components/display/fewlines/ConfirmationBox/ClickAwayListener.tsx
+++ src/components/display/fewlines/ConfirmationBox/ClickAwayListener.tsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
 export const ClickAwayListener = styled.div`
-  position: absolute;
-  height: 100%;
-  z-index: 1;
-  width: 100%;
+  position: fixed;
   left: 0;
   top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 1;
 `;
