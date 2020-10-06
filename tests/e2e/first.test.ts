6965686a780e33c2335038b1caa67528e024727c
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-10-06 10:42:51 +0200

    Improved setup

--- tests/e2e/first.test.ts
+++ tests/e2e/first.test.ts
@@ -5,48 +5,46 @@ import { config } from "./config";
 jest.setTimeout(30000);
 
 describe("Account Web Application", () => {
-  // let page: any = "";
+  let page: puppeteer.Page;
 
-  // beforeAll(async () => {
-  // await puppeteer.launch({
-  //   headless: false,
-  //   slowMo: 250, // slowdown by 250 ms
-  // });
-
-  // page = await browser.newPage();
-
-  // await page.goto("https://google.com");
-  // });
-
-  // afterEach(async () => {
-  //   await browser.close();
-  // });
-
-  it('should display "Login" text on page', async () => {
-    const browser = await puppeteer.launch({
+  beforeAll(async () => {
+    await puppeteer.launch({
       headless: config.headless,
       slowMo: config.slowMo,
     });
-    const page = await browser.newPage();
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  afterEach(async () => {
+    await jestPuppeteer.resetPage();
+  });
+
+  it('should display "Login" text on page', async () => {
+    console.log(page);
+
     await page.goto(config.accountURL, {
       waitUntil: "networkidle2",
     });
 
     await expect(page).toMatch("Login");
-
-    await browser.close();
   });
 
-  test("happy path", async () => {
-    const browser = await puppeteer.launch({
-      headless: config.headless,
-      slowMo: config.slowMo,
-    });
-    const page = await browser.newPage();
-    await page.goto(config.accountURL, {
-      waitUntil: "networkidle2",
-    });
-
-    await browser.close();
-  });
+  test("happy path", async () => {});
 });
+
+// const browser = await puppeteer.launch({
+//   headless: config.headless,
+//   slowMo: config.slowMo,
+// });
+// const page = await browser.newPage();
+// await page.goto(config.accountURL, {
+//   waitUntil: "networkidle2",
+// });
+// await browser.close();
