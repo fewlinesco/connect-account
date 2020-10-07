0c9e75ded13913eeef3fe579f6474209733a4f0b
Hamza Ali <hamza.ali@btwin.com>
2020-10-07 17:55:31 +0200

    implementing the Container in IdentityValidationForm

--- src/components/display/fewlines/IdentityValidationForm/IdentityValidationForm.tsx
+++ src/components/display/fewlines/IdentityValidationForm/IdentityValidationForm.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import { Box } from "../Box/Box";
 import { Button, ButtonVariant } from "../Button/Button";
+import { Container } from "../Container";
 import { Input } from "../Input/Input";
 import { NavigationBreadcrumbs } from "../NavigationBreadcrumbs/NavigationBreadcrumbs";
 import { IdentityTypes } from "@lib/@types/Identity";
@@ -19,98 +20,79 @@ const IdentityValidationForm: React.FC<{
 
   return (
     <Wrapper>
-      <h1>Logins</h1>
-      <NavigationBreadcrumbs
-        breadcrumbs={[
-          type.toUpperCase() === IdentityTypes.EMAIL
-            ? "Email address"
-            : "Phone number",
-          "validation",
-        ]}
-      />
-      <Form
-        method="post"
-        onSubmit={async (event) => {
-          event.preventDefault();
-
-          await verifyIdentity(validationCode);
-        }}
-      >
-        {displayAlertBar(
-          type.toUpperCase() === IdentityTypes.EMAIL
-            ? "Confirmation email has been sent"
-            : "confirmation SMS has been sent",
-        )}
-
-        <Box className="instructions">Enter the validation code below</Box>
-        <p>Validation code</p>
-        <Input
-          type="text"
-          name="value"
-          placeholder="012345"
-          value={validationCode}
-          onChange={(event) => setValidationCode(event.target.value)}
+      <Container>
+        <H1>Logins</H1>
+        <NavigationBreadcrumbs
+          breadcrumbs={[
+            type.toUpperCase() === IdentityTypes.EMAIL
+              ? "Email address"
+              : "Phone number",
+            "validation",
+          ]}
         />
+        <Form
+          method="post"
+          onSubmit={async (event) => {
+            event.preventDefault();
+
+            await verifyIdentity(validationCode);
+          }}
+        >
+          {displayAlertBar(
+            type.toUpperCase() === IdentityTypes.EMAIL
+              ? "Confirmation email has been sent"
+              : "confirmation SMS has been sent",
+          )}
+
+          <Box className="instructions">Enter the validation code below</Box>
+          <p>Validation code</p>
+          <Input
+            type="text"
+            name="value"
+            placeholder="012345"
+            value={validationCode}
+            onChange={(event) => setValidationCode(event.target.value)}
+          />
+          <Button
+            className="send-button"
+            variant={ButtonVariant.PRIMARY}
+            type="submit"
+          >{`Confirm ${type.toLowerCase()}`}</Button>
+        </Form>
         <Button
-          className="send-button"
-          variant={ButtonVariant.PRIMARY}
-          type="submit"
-        >{`Confirm ${type.toLowerCase()}`}</Button>
-      </Form>
-      <Button
-        className="discard-button"
-        variant={ButtonVariant.SECONDARY}
-        onClick={() => router.push("/account/logins/")}
-      >
-        Discard all changes
-      </Button>
-      <p className="didnt-receive-code">Didn&apos;t receive code?</p>
-      <Button className="resend-button" variant={ButtonVariant.SECONDARY}>
-        Resend confirmation code
-      </Button>
+          className="discard-button"
+          variant={ButtonVariant.SECONDARY}
+          onClick={() => router.push("/account/logins/")}
+        >
+          Discard all changes
+        </Button>
+        <DidntReceiveCode className="didnt-receive-code">
+          Didn&apos;t receive code?
+        </DidntReceiveCode>
+        <Button className="resend-button" variant={ButtonVariant.SECONDARY}>
+          Resend confirmation code
+        </Button>
+      </Container>
     </Wrapper>
   );
 };
 
 export default IdentityValidationForm;
 
+const H1 = styled.h1`
+  margin: ${({ theme }) => theme.spaces.component.s} 0
+    ${({ theme }) => theme.spaces.component.xxs};
+`;
+
+const DidntReceiveCode = styled.p`
+  margin: ${({ theme }) => theme.spaces.component.xs} 0
+    ${({ theme }) => theme.spaces.component.xxs};
+`;
+
 const Wrapper = styled.div`
-  max-width: 90%;
-  margin: 0 auto;
-
-  h1 {
-    margin: ${({ theme }) => theme.spaces.component.s} 0
-      ${({ theme }) => theme.spaces.component.xxs};
-  }
-
-  input {
-    width: 100%;
-    margin: ${({ theme }) => theme.spaces.component.xxs} 0;
-  }
-
-  button {
-    width: 100%;
-  }
-
   .instructions {
     font-weight: ${({ theme }) => theme.fontWeights.light};
     font-size: ${({ theme }) => theme.fontSizes.s};
-  }
-
-  .didnt-receive-code {
-    margin: 0 0 ${({ theme }) => theme.spaces.component.xxs} 0;
-  }
-
-  .send-button {
-    margin: ${({ theme }) => theme.spaces.component.xxs} 0;
-  }
-
-  .discard-button {
-    margin: 0 0 ${({ theme }) => theme.spaces.component.s} 0;
-  }
-
-  .resend-button {
-    margin: ${({ theme }) => theme.spaces.component.xxs} 0;
   }
 `;
 
