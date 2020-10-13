dc9a43a26d9ed1fc6984558c9526014d66bf9005
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-10-13 11:22:12 +0200

    Refacto tests

--- tests/e2e/show-identity.test.ts
+++ tests/e2e/show-identity.test.ts
@@ -7,118 +7,73 @@ import {
   write,
   press,
   screenshot,
+  waitFor,
 } from "taiko";
 
 import { config } from "@src/config";
 
-jest.setTimeout(10000);
-
 describe("Account Web Application happy path", () => {
+  const headless = true;
+  let waitTime = 0;
+  jest.setTimeout(10000);
+
+  if (!headless) {
+    jest.setTimeout(30000);
+
+    waitTime = 5000;
+  }
+
   beforeAll(async () => {
-    await openBrowser({ headless: true });
+    await openBrowser({ headless });
   });
 
   afterAll(async () => {
     await closeBrowser();
   });
 
-  test("Launch the browser and go to the login page", async () => {
-    expect.assertions(1);
+  test("Launch the browser and go to the primary email identity show", async (done) => {
+    expect.assertions(6);
 
     try {
       await goto("http://localhost:29703");
-
+      waitFor(5000);
       expect(text("Login").exists()).toBeTruthy();
+
+      await click("Login");
+      waitFor(waitTime);
+      expect(text("Email").exists()).toBeTruthy();
+
+      await write(config.connectTestAccountEmail);
+      await press("Enter");
+      waitFor(waitTime);
+      expect(text("Password").exists()).toBeTruthy();
+
+      await write(config.connectTestAccountPassword);
+      await press("Enter");
+      waitFor(waitTime);
+      expect(
+        text(
+          "Manage your logins options, including emails, phone numbers and social logins",
+        ).exists(),
+      ).toBeTruthy();
+
+      await click("Logins");
+      waitFor(waitTime);
+      expect(
+        text("Your emails, phones and social logins").exists(),
+      ).toBeTruthy();
+
+      await click("taiko@2e2.test");
+      waitFor(waitTime);
+      expect(text("Primary").exists()).toBeTruthy();
     } catch (error) {
-      await screenshot({ path: "tests/e2e/screenshots/launch.png" });
+      await screenshot({
+        path: "tests/e2e/screenshots/click_login_btn.png",
+      });
 
       throw error;
+    } finally {
+      done();
     }
-  });
-
-  describe("Perform the authentification", () => {
-    test("click the login button", async () => {
-      expect.assertions(1);
-
-      try {
-        await click("Login");
-
-        expect(text("Email").exists()).toBeTruthy();
-      } catch (error) {
-        await screenshot({
-          path: "tests/e2e/screenshots/click_login_btn.png",
-        });
-
-        throw error;
-      }
-    });
-
-    test("enter email ", async () => {
-      expect.assertions(1);
-
-      try {
-        await write(config.connectTestAccountEmail);
-        await press("Enter");
-
-        expect(text("Password").exists()).toBeTruthy();
-      } catch (error) {
-        await screenshot({ path: "tests/e2e/screenshots/enter_email.png" });
-
-        throw error;
-      }
-    });
-
-    test("enter password ", async () => {
-      expect.assertions(1);
-
-      try {
-        await write(config.connectTestAccountPassword);
-        await press("Enter");
-
-        expect(
-          text(
-            "Manage your logins options, including emails, phone numbers and social logins",
-          ).exists(),
-        ).toBeTruthy();
-      } catch (error) {
-        await screenshot({ path: "tests/e2e/screenshots/enter_password.png" });
-
-        throw error;
-      }
-    });
-
-    test("click the login anchor", async () => {
-      expect.assertions(1);
-
-      try {
-        await click("Logins");
-
-        expect(
-          text("Your emails, phones and social logins").exists(),
-        ).toBeTruthy();
-      } catch (error) {
-        await screenshot({
-          path: "tests/e2e/screenshots/click_login_btn.png",
-        });
-
-        throw error;
-      }
-    });
-
-    test("click the primary email anchor", async () => {
-      expect.assertions(1);
-
-      try {
-        await click("taiko@2e2.test");
-
-        expect(text("Primary").exists()).toBeTruthy();
-      } catch (error) {
-        await screenshot({
-          path: "tests/e2e/screenshots/click_login_btn.png",
-        });
-
-        throw error;
-      }
-    });
   });
 });
