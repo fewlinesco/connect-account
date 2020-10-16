8c06194d304853ad859c88f154c96dc124a063c5
Hamza Ali <hamza.ali@btwin.com>
2020-10-16 09:54:03 +0200

    creating DesktopNavigationBar and its story

---
+++ src/components/display/fewlines/DesktopNavigationBar/DesktopNavigationBar.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { DesktopNavigationBar } from "./DesktopNavigationBar";
+
+export default {
+  title: "components/DesktopNavigationBar",
+  component: DesktopNavigationBar,
+};
+
+export const StandardDesktopNavigationBar = (): JSX.Element => {
+  return <DesktopNavigationBar />;
+};
---
+++ src/components/display/fewlines/DesktopNavigationBar/DesktopNavigationBar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DesktopNavigationBar: React.FC = () => {
+  return <div>Hello</div>;
+};
