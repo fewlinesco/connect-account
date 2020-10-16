d6dcb19cc94e5633eee08a03044e5aef6b5e2a31
Hamza Ali <hamza.ali@btwin.com>
2020-10-16 11:01:08 +0200

    building the DesktopNavigationBar

---
+++ src/components/display/fewlines/BlackSwitchIcon/BlackSwitchIcon.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { BlackSwitchIcon } from "./BlackSwitchIcon";
+
+export default {
+  title: "components/BlackSwitchIcon",
+  component: BlackSwitchIcon,
+};
+
+export const StandardBlackSwitchIcon = (): JSX.Element => {
+  return <BlackSwitchIcon />;
+};
--- src/components/display/fewlines/DesktopNavigationBar/DesktopNavigationBar.tsx
+++ src/components/display/fewlines/DesktopNavigationBar/DesktopNavigationBar.tsx
@@ -1,5 +1,71 @@
 import React from "react";
+import styled from "styled-components";
+
+import { BlackSwitchIcon } from "../BlackSwitchIcon/BlackSwitchIcon";
+import { BlackWorldIcon } from "../BlackWorldIcon/BlackWorldIcon";
+import { HomeIcon } from "../HomeIcon/HomeIcon";
+import { KeyIcon } from "../KeyIcon/KeyIcon";
+import { LockIcon } from "../LockIcon/LockIcon";
+import { Separator } from "../Separator/Separator";
+import { deviceBreakpoints } from "@src/design-system/theme/lightTheme";
 
 export const DesktopNavigationBar: React.FC = () => {
-  return <div>Hello</div>;
+  return (
+    <Bar>
+      <ListItem>
+        <HomeIcon />
+        <div>Home</div>
+      </ListItem>
+      <ListItem>
+        <KeyIcon />
+        <div>Logins</div>
+      </ListItem>
+      <ListItem>
+        <LockIcon />
+        <div>Security</div>
+      </ListItem>
+      <Separator />
+      <SwitchLanguageItem>
+        <SwitchLanguageLabel>
+          <BlackWorldIcon />
+          <div>English</div>
+        </SwitchLanguageLabel>
+        <BlackSwitchIcon />
+      </SwitchLanguageItem>
+    </Bar>
+  );
 };
+
+const Bar = styled.div`
+  max-width: 30%;
+
+  @media ${deviceBreakpoints.m} {
+    display: none;
+  }
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${({ theme }) => theme.spaces.xs} 0 ${({ theme }) => theme.spaces.xs}
+    ${({ theme }) => theme.spaces.xs};
+
+  div {
+    margin: 0 0 0 ${({ theme }) => theme.spaces.xs};
+  }
+`;
+
+const SwitchLanguageItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: ${({ theme }) => theme.spaces.xs};
+`;
+
+const SwitchLanguageLabel = styled.div`
+  display: flex;
+  align-items: center;
+
+  div {
+    margin: 0 0 0 ${({ theme }) => theme.spaces.xs};
+  }
+`;
