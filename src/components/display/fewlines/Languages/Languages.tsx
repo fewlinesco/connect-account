3768c934e4bef889128bed05fdaea2f4d937836f
Hamza Ali <hamza.ali@btwin.com>
2020-10-19 16:40:10 +0200

    implementing list's layout

--- src/components/display/fewlines/Languages/Languages.tsx
+++ src/components/display/fewlines/Languages/Languages.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import { H1 } from "../H1/H1";
 import { Input } from "../Input/Input";
+import { Separator } from "../Separator/Separator";
 
 export const Languages: React.FC = () => {
   return (
@@ -10,22 +11,37 @@ export const Languages: React.FC = () => {
       <H1>Switch Language</H1>
       <Input />
       <Wrapper>
-        <label className="radio-container">
-          English
-          <input type="radio" name="language" value="english" />
-          <span className="circle"></span>
-        </label>
-        <label className="radio-container">
-          French
-          <input type="radio" name="language" value="french" />
-          <span className="circle"></span>
-        </label>
+        <div className="list-item">
+          <div>English</div>
+          <label className="radio-container">
+            <input type="radio" name="language" value="english" />
+            <span className="circle"></span>
+          </label>
+        </div>
+        <Separator />
+        <div className="list-item">
+          <div>French</div>
+          <label className="radio-container">
+            <input type="radio" name="language" value="french" />
+            <span className="circle"></span>
+          </label>
+        </div>
       </Wrapper>
     </div>
   );
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .list-item {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin: 10px 0;
+  }
+
   .radio-container {
     display: inline-block;
     position: relative;
@@ -40,29 +56,29 @@ const Wrapper = styled.div`
 
   .radio-container .circle {
     display: inline-block;
-    width: 25px;
-    height: 25px;
+    width: 20px;
+    height: 20px;
     background-color: white;
     position: absolute;
     left: 0;
     top: 0;
     border-radius: 50%;
-    border: 3px solid black;
+    border: 2px solid black;
   }
 
   .radio-container input:checked + .circle {
-    border: 3px solid #1825aa;
+    border: 2px solid #1825aa;
   }
 
   .radio-container input:checked + .circle:after {
     content: "";
-    height: 11px;
-    width: 11px;
+    height: 10px;
+    width: 10px;
     background-color: #1825aa;
     position: absolute;
     border-radius: 50%;
     left: 50%;
     top: 50%;
-    transform: translate(-55%, -55%);
+    transform: translate(-50%, -50%);
   }
 `;
