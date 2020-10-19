29cf4bbf1e4c5df52cd7f18587a1c1c32082091b
Hamza Ali <hamza.ali@btwin.com>
2020-10-19 15:45:03 +0200

    implementing CSS of radio button

--- src/components/display/fewlines/Languages/Languages.tsx
+++ src/components/display/fewlines/Languages/Languages.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import { H1 } from "../H1/H1";
 import { Input } from "../Input/Input";
@@ -8,14 +9,60 @@ export const Languages: React.FC = () => {
     <div>
       <H1>Switch Language</H1>
       <Input />
-      <div>
-        <label>English</label>
-        <input type="radio" name="language" value="english" />
-      </div>
-      <div>
-        <label>French</label>
-        <input type="radio" name="language" value="french" />
-      </div>
+      <Wrapper>
+        <label className="radio-container">
+          English
+          <input type="radio" name="language" value="english" />
+          <span className="circle"></span>
+        </label>
+        <label className="radio-container">
+          French
+          <input type="radio" name="language" value="french" />
+          <span className="circle"></span>
+        </label>
+      </Wrapper>
     </div>
   );
 };
+
+const Wrapper = styled.div`
+  .radio-container {
+    display: inline-block;
+    position: relative;
+    cursor: pointer;
+    user-select: none;
+    padding-left: 30px;
+  }
+
+  .radio-container input {
+    display: none;
+  }
+
+  .radio-container .circle {
+    display: inline-block;
+    width: 25px;
+    height: 25px;
+    background-color: white;
+    position: absolute;
+    left: 0;
+    top: 0;
+    border-radius: 50%;
+    border: 3px solid black;
+  }
+
+  .radio-container input:checked + .circle {
+    border: 3px solid #1825aa;
+  }
+
+  .radio-container input:checked + .circle:after {
+    content: "";
+    height: 11px;
+    width: 11px;
+    background-color: #1825aa;
+    position: absolute;
+    border-radius: 50%;
+    left: 50%;
+    top: 50%;
+    transform: translate(-55%, -55%);
+  }
+`;
