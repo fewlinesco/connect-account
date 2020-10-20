8ae38db1f835adf84ee5b4f519f51e708c13be15
Hamza Ali <hamza.ali@btwin.com>
2020-10-20 14:41:14 +0200

    using Container for H1 and Input only

--- src/components/display/fewlines/Languages/Languages.stories.tsx
+++ src/components/display/fewlines/Languages/Languages.stories.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-import { StoriesContainer } from "../StoriesContainer";
 import { Languages } from "./Languages";
 
 export default { title: "pages/Languages", component: Languages };
 
 export const StandardLanguages = (): JSX.Element => {
-  return (
-    <StoriesContainer>
-      <Languages />
-    </StoriesContainer>
-  );
+  return <Languages />;
 };
--- src/components/display/fewlines/Languages/Languages.tsx
+++ src/components/display/fewlines/Languages/Languages.tsx
@@ -3,67 +3,62 @@ import styled from "styled-components";
 
 import { H1 } from "../H1/H1";
 import { MagnifyingGlass } from "../Icons/MagnifyingGlas/MagnifyingGlass";
+import { Input } from "../Input/Input";
 import { Separator } from "../Separator/Separator";
 
 export const Languages: React.FC = () => {
   return (
-    <Container>
-      <H1>Switch Language</H1>
-      <div className="search-input">
-        <input />
-        <div className="magnifying-glass">
-          <MagnifyingGlass />
+    <>
+      <Container>
+        <H1>Switch Language</H1>
+        <div className="search-input">
+          <Input />
+          <div className="magnifying-glass">
+            <MagnifyingGlass />
+          </div>
         </div>
-      </div>
-      <Wrapper>
-        <div className="list-item">
-          <div>English</div>
+      </Container>
+      <LanguagesList>
+        <ListItem>
+          <Value>English</Value>
           <label className="radio-container">
-            <input type="radio" name="language" value="english" />
+            <input type="radio" name="language" checked={true} />
             <span className="circle"></span>
           </label>
-        </div>
+        </ListItem>
         <Separator />
-        <div className="list-item">
-          <div>French</div>
-          <label className="radio-container">
-            <input type="radio" name="language" value="french" />
-            <span className="circle"></span>
-          </label>
-        </div>
-      </Wrapper>
-    </Container>
+      </LanguagesList>
+    </>
   );
 };
 
 const Container = styled.div`
+  width: 90%;
+  margin: 0 auto;
+
   .search-input {
     display: flex;
-    align-items: center;
-    border: 0.1rem solid ${({ theme }) => theme.colors.blacks[2]};
-    border-radius: ${({ theme }) => theme.radii[0]};
+    position: relative;
+    margin-top: ${({ theme }) => theme.spaces.m};
+    margin-bottom: ${({ theme }) => theme.spaces.s};
   }
 
-  input {
-    width: 100%;
-    display: block;
+  .magnifying-glass {
+    position: absolute;
+    right: 15px;
+    bottom: 27px;
+    cursor: pointer;
   }
 `;
 
-const Wrapper = styled.div`
+const LanguagesList = styled.div`
+  width: 100%;
   display: flex;
   flex-direction: column;
 
-  .list-item {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    margin: 10px 0;
-  }
-
   .radio-container {
-    display: inline-block;
     position: relative;
+    top: -10px;
     cursor: pointer;
     user-select: none;
     padding-left: 30px;
@@ -74,7 +69,6 @@ const Wrapper = styled.div`
   }
 
   .radio-container .circle {
-    display: inline-block;
     width: 20px;
     height: 20px;
     background-color: white;
@@ -86,18 +80,31 @@ const Wrapper = styled.div`
   }
 
   .radio-container input:checked + .circle {
-    border: 2px solid #1825aa;
+    border: 2px solid ${({ theme }) => theme.colors.primary};
   }
 
   .radio-container input:checked + .circle:after {
     content: "";
     height: 10px;
     width: 10px;
-    background-color: #1825aa;
+    background-color: ${({ theme }) => theme.colors.primary};
     position: absolute;
     border-radius: 50%;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
   }
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 10px 0;
+  height: 4.8rem;
+`;
+
+const Value = styled.div`
+  margin-left: ${({ theme }) => theme.spaces.xxs};
 `;
