56a4b237920da5b296376f9fbfaa20ec07fe4d85
Hamza Ali <hamza.ali@btwin.com>
2020-10-21 18:02:40 +0200

    moving Container into AddIdentityPage

--- src/components/display/fewlines/AddIdentityForm/AddIdentityForm.tsx
+++ src/components/display/fewlines/AddIdentityForm/AddIdentityForm.tsx
@@ -2,11 +2,8 @@ import { useRouter } from "next/router";
 import React from "react";
 
 import { Button, ButtonVariant } from "../Button/Button";
-import { Container } from "../Container";
 import { Form } from "../Form/Form";
-import { H1 } from "../H1/H1";
 import { Input } from "../Input/Input";
-import { NavigationBreadcrumbs } from "../NavigationBreadcrumbs/NavigationBreadcrumbs";
 import { IdentityTypes } from "@lib/@types";
 import { InMemoryTemporaryIdentity } from "@src/@types/InMemoryTemporaryIdentity";
 
@@ -28,14 +25,7 @@ export const AddIdentityForm: React.FC<AddIdentityFormProps> = ({
   const router = useRouter();
 
   return (
-    <Container>
-      <H1>Logins</H1>
-      <NavigationBreadcrumbs
-        breadcrumbs={[
-          type === IdentityTypes.EMAIL ? "Email address" : "Phone number",
-          "new",
-        ]}
-      />
+    <>
       <Form
         onSubmit={async () => {
           await addIdentity(identity);
@@ -68,6 +58,6 @@ export const AddIdentityForm: React.FC<AddIdentityFormProps> = ({
       >
         Cancel
       </Button>
-    </Container>
+    </>
   );
 };
