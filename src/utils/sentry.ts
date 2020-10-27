5c018694caa466585b6eb60eaa33b6a42ee454c4
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-10-27 16:16:32 +0100

    Added sentry env

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -8,6 +8,7 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     enabled: process.env.NODE_ENV !== "test",
+    environment: process.env.NODE_ENV,
   });
 }
 
