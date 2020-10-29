6f552ed9e0aaa5330b5038349ac71390e2065619
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-10-29 20:21:17 +0100

    Fix wording

--- src/components/display/fewlines/ConfirmationBox/PrimaryConfirmationBox.tsx
+++ src/components/display/fewlines/ConfirmationBox/PrimaryConfirmationBox.tsx
@@ -3,14 +3,12 @@ import styled from "styled-components";
 
 import { Button, ButtonVariant } from "../Button/Button";
 import { ConfirmationBox } from "./ConfirmationBox";
-import { IdentityTypes } from "@lib/@types";
 
 export const PrimaryConfirmationBox = (
   primaryConfirmationBoxOpen: boolean,
   preventPrimaryAnimation: boolean,
   setPrimaryConfirmationBoxOpen: React.Dispatch<React.SetStateAction<boolean>>,
   value: string,
-  type: IdentityTypes,
 ): JSX.Element => {
   return (
     <ConfirmationBox
@@ -20,16 +18,16 @@ export const PrimaryConfirmationBox = (
     >
       <>
         <PrimaryConfirmationText>
-          You are about to replace mail@mail.com as your main address
+          You are about to set {value} as primary.
         </PrimaryConfirmationText>
-        <Button variant={ButtonVariant.PRIMARY}>Set {value} as my main</Button>
+        <Button variant={ButtonVariant.PRIMARY}>Confirm</Button>
         <Button
           variant={ButtonVariant.SECONDARY}
           onClick={() => {
             setPrimaryConfirmationBoxOpen(false);
           }}
         >
-          Keep mail@mail.co as my primary {type.toLowerCase()}
+          Cancel
         </Button>
       </>
     </ConfirmationBox>
