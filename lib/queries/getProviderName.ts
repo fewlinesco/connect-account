44d40c9d64ec1adfa57f7b9f8271cff809f6e4d1
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-10-29 10:11:33 +0100

    Added provider name query

---
+++ lib/queries/getProviderName.ts
@@ -0,0 +1,25 @@
+import { FetchResult } from "apollo-link";
+import gql from "graphql-tag";
+
+import { fetchManagement } from "@src/utils/fetchManagement";
+
+const GET_PROVIDER_NAME_QUERY = gql`
+  query getProviderName {
+    provider {
+      name
+    }
+  }
+`;
+
+export async function getProviderName(): Promise<
+  FetchResult<{ provider: { name: string } }>
+> {
+  const operation = {
+    query: GET_PROVIDER_NAME_QUERY,
+    variables: {},
+  };
+
+  return fetchManagement(operation) as FetchResult<{
+    provider: { name: string };
+  }>;
+}
