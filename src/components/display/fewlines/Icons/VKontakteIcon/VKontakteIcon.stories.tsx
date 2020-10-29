a7144e58c8cb2488398f81122a23e3c8426ecf21
Pauline Roche <paulineroche@Paulines-MBP.fewlines.local>
2020-10-29 11:24:42 +0100

    adding social identities icons in svg components

---
+++ src/components/display/fewlines/Icons/VKontakteIcon/VKontakteIcon.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { VKontakteIcon } from "./VKontakteIcon";
+
+export default {
+  title: "icons/social/VKontakte Icon",
+  component: VKontakteIcon,
+};
+
+export const DefaultVKontakteIcon = (): JSX.Element => {
+  return <VKontakteIcon />;
+};
