dd5c3479547da8362612955b8e5289845ff45184
Hamza Ali <hamza.ali@btwin.com>
2020-10-30 10:23:48 +0100

    using position sticky for confirmation box

--- src/components/display/fewlines/IdentityOverview/IdentityOverview.tsx
+++ src/components/display/fewlines/IdentityOverview/IdentityOverview.tsx
@@ -29,28 +29,29 @@ export const IdentityOverview: React.FC<IdentityOverviewProps> = ({
   const { primary, status, type, value } = identity;
 
   return (
-    <>
-      <Box>
-        <Flex>
-          <Value>{value}</Value>
-        </Flex>
-        {primary && status === "validated" && <PrimaryBadge />}
-        {status === "validated" ? (
-          <React.Fragment />
-        ) : (
-          <AwaitingValidationBadge />
+    <Column>
+      <PageContent>
+        <Box>
+          <Flex>
+            <Value>{value}</Value>
+          </Flex>
+          {primary && status === "validated" && <PrimaryBadge />}
+          {status === "validated" ? (
+            <React.Fragment />
+          ) : (
+            <AwaitingValidationBadge />
+          )}
+        </Box>
+        {status === "unvalidated" && (
+          <Link href={`/account/logins/${type}/validation`}>
+            <a>
+              <Button variant={ButtonVariant.PRIMARY}>
+                proceed to validation
+              </Button>
+            </a>
+          </Link>
         )}
-      </Box>
-      {status === "unvalidated" && (
-        <Link href={`/account/logins/${type}/validation`}>
-          <a>
-            <Button variant={ButtonVariant.PRIMARY}>
-              proceed to validation
-            </Button>
-          </a>
-        </Link>
-      )}
-      {/* {status === "validated" && (
+        {/* {status === "validated" && (
         <Link href={`/account/logins/${type}/${id}/update`}>
           <a>
             <Button variant={ButtonVariant.PRIMARY}>
@@ -60,35 +61,40 @@ export const IdentityOverview: React.FC<IdentityOverviewProps> = ({
           </a>
         </Link>
       )} */}
-      {!primary && status === "validated" && (
-        <Button
-          variant={ButtonVariant.SECONDARY}
-          onClick={() => {
-            setPreventAnimation(false);
-            setConfirmationBoxContent(
-              PrimaryConfirmationBoxContent(setConfirmationBoxOpen, value),
-            );
-            setConfirmationBoxOpen(true);
-          }}
-        >
-          Make this my primary {type.toLowerCase()}
-        </Button>
-      )}
-      {!primary && (
-        <Button
-          variant={ButtonVariant.GHOST}
-          onClick={() => {
-            setPreventAnimation(false);
-            setConfirmationBoxContent(
-              DeleteConfirmationBoxContent(setConfirmationBoxOpen, value, type),
-            );
-            setConfirmationBoxOpen(true);
-          }}
-        >
-          Delete this{" "}
-          {type === IdentityTypes.PHONE ? "phone number" : "email address"}
-        </Button>
-      )}
+        {!primary && status === "validated" && (
+          <Button
+            variant={ButtonVariant.SECONDARY}
+            onClick={() => {
+              setPreventAnimation(false);
+              setConfirmationBoxContent(
+                PrimaryConfirmationBoxContent(setConfirmationBoxOpen, value),
+              );
+              setConfirmationBoxOpen(true);
+            }}
+          >
+            Make this my primary {type.toLowerCase()}
+          </Button>
+        )}
+        {!primary && (
+          <Button
+            variant={ButtonVariant.GHOST}
+            onClick={() => {
+              setPreventAnimation(false);
+              setConfirmationBoxContent(
+                DeleteConfirmationBoxContent(
+                  setConfirmationBoxOpen,
+                  value,
+                  type,
+                ),
+              );
+              setConfirmationBoxOpen(true);
+            }}
+          >
+            Delete this{" "}
+            {type === IdentityTypes.PHONE ? "phone number" : "email address"}
+          </Button>
+        )}
+      </PageContent>
       <ConfirmationBox
         open={confirmationBoxOpen}
         setOpen={setConfirmationBoxOpen}
@@ -96,9 +102,20 @@ export const IdentityOverview: React.FC<IdentityOverviewProps> = ({
       >
         {confirmationBoxContent}
       </ConfirmationBox>
-    </>
+    </Column>
   );
 };
+
+const Column = styled.div`
+  height: calc(100% - 11rem);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const PageContent = styled.div`
+  display: block;
+`;
 
 const Value = styled.p`
   font-weight: ${({ theme }) => theme.fontWeights.bold};
