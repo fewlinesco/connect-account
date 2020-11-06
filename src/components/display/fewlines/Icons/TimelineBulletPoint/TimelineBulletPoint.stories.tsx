adaab927956b82cd948d3ec888c6d0a97a5b29b2
Hamza Ali <hamza.ali@btwin.com>
2020-11-06 16:09:43 +0100

    creating timeline bullet point and its story

---
+++ src/components/display/fewlines/Icons/TimelineBulletPoint/TimelineBulletPoint.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { TimelineBulletPoint } from "./TimelineBulletPoint";
+
+export default {
+  title: "icons/Timeline Bullet Point",
+  component: TimelineBulletPoint,
+};
+
+export const StandardTimelineBulletPoint = (): JSX.Element => {
+  return <TimelineBulletPoint />;
+};
