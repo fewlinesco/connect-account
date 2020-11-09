a007fa8ec1cdc675ed781e76076d7abef0f798df
Hamza Ali <hamza.ali@btwin.com>
2020-11-09 10:21:43 +0100

    fixing css for timeline

--- src/components/display/fewlines/Timeline/Timeline.tsx
+++ src/components/display/fewlines/Timeline/Timeline.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import styled from "styled-components";
 
+import { TimelineBulletPoint } from "../Icons/TimelineBulletPoint/TimelineBulletPoint";
+
 export const Timeline: React.FC = () => {
-  return <Content />;
+  return (
+    <>
+      <BulletPointContainer>
+        <TimelineBulletPoint />
+      </BulletPointContainer>
+      <TimelineContainer>
+        <Line />
+      </TimelineContainer>
+    </>
+  );
 };
 
-const Content = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
+const BulletPointContainer = styled.div`
+  display: inline-block;
+`;
+
+const TimelineContainer = styled.div`
+  display: block;
+  width: 0.8rem;
+  height: 100%;
+  margin: 0 0 0 0.4rem;
+`;
+
+const Line = styled.div`
   height: calc(100% + ${({ theme }) => theme.spaces.s});
   border-left: 1px solid ${({ theme }) => theme.colors.separator};
 `;
