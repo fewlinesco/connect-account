e795828eae7b592a14407aecc4eb2194207757b0
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-11-13 08:34:23 +0100

    Removed logs

--- src/utils/getUser.ts
+++ src/utils/getUser.ts
@@ -6,18 +6,10 @@ export async function getUser(cookie: string): Promise<MongoUser> {
   const route = "/api/auth-connect/get-user";
   const absoluteURL = new URL(route, config.connectDomain).toString();
 
-  console.log("Flag: before first mocked fetch");
-
   const { user } = await fetch(absoluteURL, {
     method: HttpVerbs.GET,
     headers: { cookie },
-  }).then((response) => {
-    console.log(
-      "First mock response, should be 299d268e-3e19-4486-9be7-29c539d241ac",
-      response,
-    );
-    return response.json();
-  });
+  }).then((response) => response.json());
 
   return user;
 }
