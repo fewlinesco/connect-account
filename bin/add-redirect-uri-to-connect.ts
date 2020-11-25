bc42f15cfd7282acf5bcd2d1194567bf1ff1faef
Oluane <rochepauline6@gmail.com>
2020-11-25 15:17:27 +0100

    check uri existence before adding to management

--- bin/add-redirect-uri-to-connect.ts
+++ bin/add-redirect-uri-to-connect.ts
@@ -20,7 +20,9 @@ async function addRedirectURIToConnect(): Promise<void> {
 
   const vercelDeployment = githubActionsContext.deployment_status;
 
-  if (vercelDeployment.state === "success") {
+  const deploymentStates = ["success", "pending", "in_progress", "queued"];
+
+  if (deploymentStates.includes(vercelDeployment.state)) {
     const testApp = await getApplication(
       process.env.CONNECT_ACCOUNT_TEST_APP_ID,
     ).then(({ errors, data }) => {
@@ -35,20 +37,25 @@ async function addRedirectURIToConnect(): Promise<void> {
       return data.provider.application;
     });
 
-    const updatedTestApp = {
-      ...testApp,
-      redirectUris: [...testApp.redirectUris, vercelDeployment.target_url],
-    };
+    if (
+      !vercelDeployment.target_url.includes("storybook") &&
+      !testApp.redirectUris.includes(vercelDeployment.target_url)
+    ) {
+      const updatedTestApp = {
+        ...testApp,
+        redirectUris: [...testApp.redirectUris, vercelDeployment.target_url],
+      };
 
-    await updateApplication(updatedTestApp).then(({ errors, data }) => {
-      if (errors) {
-        throw errors;
-      }
+      await updateApplication(updatedTestApp).then(({ errors, data }) => {
+        if (errors) {
+          throw errors;
+        }
 
-      if (!data) {
-        throw new Error("update error");
-      }
-    });
+        if (!data) {
+          throw new Error("update error");
+        }
+      });
+    }
   }
 }
 
