dc5400b763c7fa63a624be6adac7efc0b538dc4f
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-12-01 09:55:09 +0100

    Fixed impropoer access control on mark identity as primary endpoint

--- src/pages/api/auth-connect/mark-identity-as-primary.ts
+++ src/pages/api/auth-connect/mark-identity-as-primary.ts
@@ -2,6 +2,9 @@ import { HttpStatus } from "@fwl/web";
 import { Handler } from "next-iron-session";
 
 import { markIdentityAsPrimary } from "@lib/commands/markIdentityAsPrimary";
+import { getIdentities } from "@lib/queries/getIdentities";
+import { UserCookie } from "@src/@types/UserCookie";
+import { ExtendedRequest } from "@src/@types/core/ExtendedRequest";
 import { withAuth } from "@src/middlewares/withAuth";
 import { withLogger } from "@src/middlewares/withLogger";
 import { withSentry } from "@src/middlewares/withSentry";
@@ -9,20 +12,31 @@ import { withSession } from "@src/middlewares/withSession";
 import { wrapMiddlewares } from "@src/middlewares/wrapper";
 import Sentry, { addRequestScopeToSentry } from "@src/utils/sentry";
 
-const handler: Handler = async (request, response) => {
+const handler: Handler = async (request: ExtendedRequest, response) => {
   addRequestScopeToSentry(request);
 
   try {
     if (request.method === "POST") {
       const { identityId } = request.body;
 
-      return markIdentityAsPrimary(identityId).then((data) => {
-        response.statusCode = HttpStatus.OK;
+      const userCookie = request.session.get<UserCookie>(
+        "user-session",
+      ) as UserCookie;
+      const managementResponse = await getIdentities(userCookie.sub);
+      const identities = managementResponse.data?.provider.user.identities;
 
-        response.setHeader("Content-type", "application/json");
+      const isAuthorized = identities ? identities.includes(identityId) : false;
 
-        response.json({ data });
-      });
+      if (isAuthorized) {
+        return markIdentityAsPrimary(identityId).then((data) => {
+          response.statusCode = HttpStatus.OK;
+          response.setHeader("Content-type", "application/json");
+          response.json({ data });
+        });
+      }
+
+      response.statusCode = HttpStatus.FORBIDDEN;
+      response.end();
     }
   } catch (error) {
     Sentry.withScope((scope) => {
@@ -33,7 +47,8 @@ const handler: Handler = async (request, response) => {
 
   response.statusCode = HttpStatus.METHOD_NOT_ALLOWED;
 
-  return Promise.reject();
+  // TODO: better deal with this error
+  return Promise.reject(new Error("Method not allowed"));
 };
 
 export default wrapMiddlewares(
