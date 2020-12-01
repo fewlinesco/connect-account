65f9412b4e91d40e401d598930b45d52aa9df490
Oluane <rochepauline6@gmail.com>
2020-12-01 18:00:08 +0100

    refacto of NeutralLink to fc component (#142)
    Co-authored-by: Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>

--- src/components/display/fewlines/NeutralLink.tsx
+++ src/components/display/fewlines/NeutralLink.tsx
@@ -1,6 +1,29 @@
+import Link, { LinkProps } from "next/link";
+import React from "react";
 import style from "styled-components";
 
-export const NeutralLink = style.a`
+interface ExtendedLinkProps extends LinkProps {
+  className?: string;
+  onClick?: () => void;
+}
+
+export const NeutralLink: React.FC<ExtendedLinkProps> = ({
+  href,
+  as,
+  className,
+  onClick,
+  children,
+}) => {
+  return (
+    <Link href={href} as={as} passHref>
+      <NeutralAnchor className={className} onClick={onClick}>
+        {children}
+      </NeutralAnchor>
+    </Link>
+  );
+};
+
+const NeutralAnchor = style.a`
   text-decoration: none;
   color: ${({ theme }) => theme.colors.black};
 
