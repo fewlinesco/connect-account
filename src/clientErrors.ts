4bcac7f6f1e27a853f82c79869ab0b73fa24af0c
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2020-12-04 10:05:25 +0100

    Handle errors related to adding an Identity. (#150)

--- src/clientErrors.ts
+++ src/clientErrors.ts
@@ -5,3 +5,30 @@ export class ErrorVerifyingValidationCode extends Error {}
 export class ErrorSettingPassword extends Error {}
 
 export class NoIdentityFound extends Error {}
+
+export class IdentityAlreadyUsed extends Error {
+  readonly message: string;
+
+  constructor(message: string) {
+    super();
+    this.message = message;
+  }
+}
+
+export class IdentityInputValueCantBeBlank extends Error {
+  readonly message: string;
+
+  constructor(message: string) {
+    super();
+    this.message = message;
+  }
+}
+
+export class PhoneNumberInputValueShouldBeANumber extends Error {
+  readonly message: string;
+
+  constructor(message: string) {
+    super();
+    this.message = message;
+  }
+}
