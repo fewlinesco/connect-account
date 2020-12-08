06e17eb13d7629e14bc41cd18dc98f820beb8828
Oluane <rochepauline6@gmail.com>
2020-12-08 10:38:54 +0100

    fix unhandled identity type error + unit test

--- src/utils/sortIdentities.ts
+++ src/utils/sortIdentities.ts
@@ -2,6 +2,7 @@ import { getIdentityType } from "./getIdentityType";
 import { Identity, IdentityTypes } from "@lib/@types";
 import type { ProviderUser } from "@lib/@types/ProviderUser";
 import type { SortedIdentities } from "@src/@types/SortedIdentities";
+import { UnhandledIdentityType } from "@src/clientErrors";
 
 export function sortIdentities(fetchedData: {
   data?: { provider: ProviderUser } | null;
@@ -14,28 +15,36 @@ export function sortIdentities(fetchedData: {
     const identities: Identity[] = fetchedData.data.provider.user.identities;
 
     identities.forEach((identity) => {
-      const identityPattern = {
-        id: identity.id,
-        primary: identity.primary,
-        status: identity.status,
-        value: identity.value,
-      };
+      try {
+        const identityPattern = {
+          id: identity.id,
+          primary: identity.primary,
+          status: identity.status,
+          value: identity.value,
+        };
 
-      if (getIdentityType(identity.type) === IdentityTypes.EMAIL) {
-        emailIdentities.push({
-          ...identityPattern,
-          type: IdentityTypes.EMAIL,
-        });
-      } else if (getIdentityType(identity.type) === IdentityTypes.PHONE) {
-        phoneIdentities.push({
-          ...identityPattern,
-          type: IdentityTypes.PHONE,
-        });
-      } else if (getIdentityType(identity.type) !== IdentityTypes.PROVIDER) {
-        socialIdentities.push({
-          ...identityPattern,
-          type: getIdentityType(identity.type),
-        });
+        if (getIdentityType(identity.type) === IdentityTypes.EMAIL) {
+          emailIdentities.push({
+            ...identityPattern,
+            type: IdentityTypes.EMAIL,
+          });
+        } else if (getIdentityType(identity.type) === IdentityTypes.PHONE) {
+          phoneIdentities.push({
+            ...identityPattern,
+            type: IdentityTypes.PHONE,
+          });
+        } else if (getIdentityType(identity.type) !== IdentityTypes.PROVIDER) {
+          socialIdentities.push({
+            ...identityPattern,
+            type: getIdentityType(identity.type),
+          });
+        }
+      } catch (error) {
+        if (error instanceof UnhandledIdentityType) {
+          return;
+        } else {
+          throw error;
+        }
       }
     });
   }
