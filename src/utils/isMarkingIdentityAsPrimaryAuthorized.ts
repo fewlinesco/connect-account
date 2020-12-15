16c91e26414a9d025b44db83eca718a1af7b101b
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-12-15 14:36:58 +0100

    getIdentities

--- src/utils/isMarkingIdentityAsPrimaryAuthorized.ts
+++ src/utils/isMarkingIdentityAsPrimaryAuthorized.ts
@@ -1,12 +1,22 @@
 import { getIdentities } from "@lib/queries/getIdentities";
+import { NoIdentityFound } from "@src/clientErrors";
+import { GraphqlErrors } from "@src/errors";
 
 export async function isMarkingIdentityAsPrimaryAuthorized(
   sub: string,
   identityId: string,
 ): Promise<boolean> {
-  const managementResponse = await getIdentities(sub);
+  const identities = await getIdentities(sub).then(({ errors, data }) => {
+    if (errors) {
+      throw new GraphqlErrors(errors);
+    }
 
-  const identities = managementResponse.data?.provider.user.identities;
+    if (!data) {
+      throw new NoIdentityFound();
+    }
+
+    return data.provider.user.identities;
+  });
 
   return identities
     ? identities.some((identity) => identity.id === identityId)
