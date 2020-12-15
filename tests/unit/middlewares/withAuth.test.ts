7983703e12cf4d1435fb19e9fe76400d772e3908
Fuzznimp <montaigne.anthony.dev@gmail.com>
2020-12-15 09:59:06 +0100

    Refacto tests

--- tests/unit/middlewares/withAuth.test.ts
+++ tests/unit/middlewares/withAuth.test.ts
@@ -8,12 +8,40 @@ import * as refreshTokensFlow from "@lib/commands/refreshTokensFlow";
 import { ExtendedRequest } from "@src/@types/core/ExtendedRequest";
 import { Handler } from "@src/@types/core/Handler";
 import * as getAndPutUser from "@src/commands/getAndPutUser";
-import { config } from "@src/config";
+import { config, oauth2Client } from "@src/config";
 import { withAuth } from "@src/middlewares/withAuth";
 import { withSession } from "@src/middlewares/withSession";
 import { wrapMiddlewares } from "@src/middlewares/wrapper";
 import * as getDBUserFromSub from "@src/queries/getDBUserFromSub.ts";
 import * as decryptVerifyAccessToken from "@src/workflows/decryptVerifyAccessToken";
+
+jest.mock("@src/config", () => {
+  const configFile = jest.requireActual("@src/config");
+
+  return {
+    config: {
+      connectAccountSessionSalt: "#*b+x3ZXE3-h[E+Q5YC5`jr~y%CA~R-[",
+      connectOpenIdConfigurationUrl: "",
+      connectApplicationClientId: "",
+      connectApplicationClientSecret: "c9ab0fdc-b2dc-47ad-933b-87cf1b180ab5",
+      connectAccountRedirectUri: "",
+      connectAudience: "",
+      connectApplicationScopes: "",
+      connectProviderUrl: "http://foo.test",
+      connectAccountURL: "http://foo.test",
+      dynamoRegion: "eu-west-3",
+    },
+    oauth2Client: configFile.oauth2Client,
+  };
+});
+
+const spiedOnVerifyJWT = jest
+  .spyOn(oauth2Client, "verifyJWT")
+  .mockImplementation(async () => {
+    return {
+      sub: "2a14bdd2-3628-4912-a76e-fd514b5c27a8",
+    };
+  });
 
 const spiedOnDecryptVerifyAccessToken = jest
   .spyOn(decryptVerifyAccessToken, "decryptVerifyAccessToken")
@@ -45,34 +73,6 @@ const spiedOnGetAndPutUser = jest
   .mockImplementation(async () => {
     return;
   });
-
-let mockedVerifyJWTCall = 0;
-
-jest.mock("@src/config", () => {
-  return {
-    config: {
-      connectAccountSessionSalt: "#*b+x3ZXE3-h[E+Q5YC5`jr~y%CA~R-[",
-      connectOpenIdConfigurationUrl: "",
-      connectApplicationClientId: "",
-      connectApplicationClientSecret: "c9ab0fdc-b2dc-47ad-933b-87cf1b180ab5",
-      connectAccountRedirectUri: "",
-      connectAudience: "",
-      connectApplicationScopes: "",
-      connectProviderUrl: "http://foo.test",
-      connectAccountURL: "http://foo.test",
-      dynamoRegion: "eu-west-3",
-    },
-    oauth2Client: {
-      verifyJWT: async () => {
-        mockedVerifyJWTCall = +1;
-
-        return {
-          sub: "2a14bdd2-3628-4912-a76e-fd514b5c27a8",
-        };
-      },
-    },
-  };
-});
 
 async function sealJWS(access_token: string, salt?: string): Promise<string> {
   return seal(
@@ -149,7 +149,7 @@ describe("withAuth", () => {
   });
 
   test("should redirect to the login flow if no UserCookie is provided", async (done) => {
-    expect.assertions(3);
+    expect.assertions(2);
 
     const access_token = generateHS256JWS();
     const sealedJWS = await sealJWS(
@@ -169,7 +169,6 @@ describe("withAuth", () => {
 
     expect(mockedResponse.statusCode).toEqual(HttpStatus.TEMPORARY_REDIRECT);
     expect(spiedOnDecryptVerifyAccessToken).not.toHaveBeenCalled();
-    expect(mockedVerifyJWTCall).toEqual(1);
 
     done();
   });
@@ -197,7 +196,7 @@ describe("withAuth", () => {
         throw new TokenExpiredError();
       });
 
-      spiedOnGetDBUserFromSub.mockImplementation(async () => {
+      spiedOnGetDBUserFromSub.mockImplementationOnce(async () => {
         return null;
       });
 
@@ -210,7 +209,7 @@ describe("withAuth", () => {
       expect(spiedOnDecryptVerifyAccessToken).toHaveBeenCalled();
       expect(spiedOnGetDBUserFromSub).toHaveBeenCalled();
       expect(spiedOnRefreshTokensFlow).not.toHaveBeenCalled();
-      expect(mockedVerifyJWTCall).toEqual(1);
+      expect(spiedOnVerifyJWT).not.toHaveBeenCalled();
       expect(spiedOnGetAndPutUser).not.toHaveBeenCalled();
       expect(mockedResponse.statusCode).toEqual(HttpStatus.TEMPORARY_REDIRECT);
       expect(mockedResponse.getHeader("location")).toBe("/");
@@ -218,8 +217,8 @@ describe("withAuth", () => {
       done();
     });
 
-    test.only("should refresh the user tokens if a `TokenExpiredError` exception is thrown and a user is provided, and should not redirect", async (done) => {
-      expect.assertions(6);
+    test("should refresh the user tokens if a `TokenExpiredError` exception is thrown and a user is provided, and should not redirect", async (done) => {
+      expect.assertions(7);
 
       const access_token = generateHS256JWS({
         ...defaultPayload,
@@ -251,6 +250,7 @@ describe("withAuth", () => {
       expect(spiedOnDecryptVerifyAccessToken).toHaveBeenCalled();
       expect(spiedOnGetDBUserFromSub).toHaveBeenCalled();
       expect(spiedOnRefreshTokensFlow).toHaveBeenCalled();
+      expect(spiedOnVerifyJWT).toHaveBeenCalled();
       expect(spiedOnGetAndPutUser).toHaveBeenCalled();
       expect(mockedResponse.statusCode).toEqual(HttpStatus.OK);
       expect(mockedResponse.getHeader("location")).toBe("referer/url");
