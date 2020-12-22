9b591bf8a7266a798efb4a705f45d2e146b5f3b7
Oluane <rochepauline6@gmail.com>
2020-12-22 11:10:18 +0100

    fixing storybook issue with next links

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,28 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from "../src/design-system/theme";
 import { GlobalStyle } from "../src/design-system/globals/globalStyle";
+import Router from 'next/router';
+import { RouterContext } from 'next/dist/next-server/lib/router-context';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-const withThemeProvider=(Story,context)=>{
+const mockedNextRouter = Router.router = {
+  push: () => {},
+  replace: () => {},
+  prefetch: () => new Promise((resolve) => resolve()),
+};
+
+const withMockRouterAndThemeProvider=(Story,context)=>{
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Story {...context} />
-    </ThemeProvider>
+    <RouterContext.Provider value={{...mockedNextRouter}}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Story {...context} />
+      </ThemeProvider>
+    </RouterContext.Provider>
   )
 }
-export const decorators = [withThemeProvider];
+
+export const decorators = [withMockRouterAndThemeProvider];
