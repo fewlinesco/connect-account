3d50e95a2da77f4d174fd5f7f0db82cd575b0c73
Oluane <rochepauline6@gmail.com>
2021-01-11 16:21:08 +0100

    add identity page test revamp

--- tests/pages/add-identity-page.test.tsx
+++ tests/pages/add-identity-page.test.tsx
@@ -1,30 +1,9 @@
-import { mount } from "enzyme";
-import { enableFetchMocks } from "jest-fetch-mock";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
+import { render, screen } from "../config/testing-library-config";
 import { IdentityTypes } from "@lib/@types";
-import { Button, ButtonVariant } from "@src/components/button/button";
-import { FakeButton } from "@src/components/fake-button/fake-button";
-import { Form } from "@src/components/form/form";
-import { Input } from "@src/components/input/input";
-import {
-  NavigationBreadcrumbs,
-  Breadcrumbs,
-} from "@src/components/navigation-breadcrumbs/navigation-breadcrumbs";
-import { AccountApp } from "@src/pages/_app";
 import AddIdentityPage from "@src/pages/account/logins/[type]/new";
-import * as fetchJson from "@src/utils/fetch-json";
-
-enableFetchMocks();
-
-jest.mock("@src/config", () => {
-  return {
-    config: {
-      connectApplicationClientSecret: "foo-bar",
-      connectAccountSessionSalt: ".*b+x3ZXE3-h[E+Q5YC5`jr~y%CA~R-[",
-    },
-  };
-});
 
 jest.mock("@src/db-client", () => {
   return {
@@ -37,150 +16,79 @@ jest.mock("@src/db-client", () => {
 });
 
 describe("AddIdentityPage", () => {
-  test("it should display navigation breadcrumbs properly for emails", () => {
-    const component = mount(
-      <AccountApp>
-        <AddIdentityPage type={IdentityTypes.EMAIL} />
-      </AccountApp>,
-    );
+  describe("Identity type : EMAIL", () => {
+    it("should render proper email breadcrumbs", () => {
+      render(<AddIdentityPage type={IdentityTypes.EMAIL} />);
 
-    const navigationBreadCrumbs = component.find(NavigationBreadcrumbs);
-    expect(navigationBreadCrumbs).toHaveLength(1);
-    expect(
-      navigationBreadCrumbs.contains(
-        <Breadcrumbs>Email address | new</Breadcrumbs>,
-      ),
-    ).toEqual(true);
-  });
-
-  test("it should display navigation breadcrumbs properly for phones", () => {
-    const component = mount(
-      <AccountApp>
-        <AddIdentityPage type={IdentityTypes.PHONE} />
-      </AccountApp>,
-    );
-
-    const navigationBreadCrumbs = component.find(NavigationBreadcrumbs);
-    expect(navigationBreadCrumbs).toHaveLength(1);
-    expect(
-      navigationBreadCrumbs.contains(
-        <Breadcrumbs>Phone number | new</Breadcrumbs>,
-      ),
-    ).toEqual(true);
-  });
-
-  test("it should display the form properly for emails", () => {
-    const component = mount(
-      <AccountApp>
-        <AddIdentityPage type={IdentityTypes.EMAIL} />
-      </AccountApp>,
-    );
-
-    const addIdentityInput = component
-      .find(Form)
-      .find(Input)
-      .find('[type="email"]')
-      .hostNodes();
-    expect(addIdentityInput).toHaveLength(1);
-    expect(addIdentityInput.props().placeholder).toEqual("Enter your email");
-
-    const markAsPrimaryCheckbox = component
-      .find(Form)
-      .find(Input)
-      .find('[type="checkbox"]')
-      .hostNodes();
-    expect(markAsPrimaryCheckbox).toHaveLength(1);
-
-    const addButton = component
-      .find(Form)
-      .find(Button)
-      .find({ variant: ButtonVariant.PRIMARY });
-
-    const cancelButton = component
-      .find(FakeButton)
-      .find({ variant: ButtonVariant.SECONDARY, as: "div" });
-
-    expect(addButton).toHaveLength(1);
-    expect(addButton.text()).toEqual("Add email");
-    expect(cancelButton).toHaveLength(1);
-    expect(cancelButton.text()).toEqual("Cancel");
-  });
-
-  test("it should display the form properly for phones", () => {
-    const component = mount(
-      <AccountApp>
-        <AddIdentityPage type={IdentityTypes.PHONE} />
-      </AccountApp>,
-    );
-
-    const addIdentityInput = component
-      .find(Form)
-      .find(Input)
-      .find('[type="text"]')
-      .hostNodes();
-    expect(addIdentityInput).toHaveLength(1);
-    expect(addIdentityInput.props().placeholder).toEqual("Enter your phone");
-
-    const markAsPrimaryCheckbox = component
-      .find(Form)
-      .find(Input)
-      .find('[type="checkbox"]')
-      .hostNodes();
-    expect(markAsPrimaryCheckbox).toHaveLength(1);
-
-    const addButton = component
-      .find(Form)
-      .find(Button)
-      .find({ variant: ButtonVariant.PRIMARY });
-
-    const cancelButton = component
-      .find(FakeButton)
-      .find({ variant: ButtonVariant.SECONDARY, as: "div" });
-
-    expect(addButton).toHaveLength(1);
-    expect(addButton.text()).toEqual("Add phone");
-    expect(cancelButton).toHaveLength(1);
-    expect(cancelButton.text()).toEqual("Cancel");
-  });
-
-  test("it should call `send-identity-validation-code` API page on submit", () => {
-    const spyDate = jest.spyOn(Date, "now").mockReturnValue(1572393600000);
-
-    const component = mount(
-      <AccountApp>
-        <AddIdentityPage type={IdentityTypes.EMAIL} />
-      </AccountApp>,
-    );
-
-    const newIdentityInput = component
-      .find(Input)
-      .find('[type="email"]')
-      .hostNodes();
-
-    newIdentityInput.simulate("change", {
-      target: { value: "test3@test.test " },
+      expect(screen.getByText("Email address | new")).toBeInTheDocument();
     });
 
-    const form = component.find(Form);
+    it("should render proper email form elements ", () => {
+      render(<AddIdentityPage type={IdentityTypes.EMAIL} />);
 
-    const fetchJsonMethod = jest.spyOn(fetchJson, "fetchJson");
+      const addIdentityInput = screen.getByRole("textbox");
 
-    form.simulate("submit");
+      expect(addIdentityInput).toBeVisible();
+      userEvent.type(addIdentityInput, "test@test.test");
+      expect(addIdentityInput).toHaveDisplayValue("test@test.test");
 
-    expect(fetchJsonMethod).toHaveBeenCalledWith(
-      "/api/auth-connect/send-identity-validation-code",
-      "POST",
-      {
-        callbackUrl: "/",
-        identityInput: {
-          expiresAt: 1572393600000 + 300000,
-          primary: false,
-          type: IdentityTypes.EMAIL,
-          value: "test3@test.test ",
-        },
-      },
-    );
+      const markAsPrimaryCheckbox = screen.getByRole("checkbox", {
+        name: "Mark this identity as my primary one",
+      });
 
-    spyDate.mockRestore();
+      expect(markAsPrimaryCheckbox).toBeInTheDocument();
+      expect(markAsPrimaryCheckbox).not.toBeChecked();
+      userEvent.click(markAsPrimaryCheckbox);
+      expect(markAsPrimaryCheckbox).toBeChecked();
+
+      expect(
+        screen.getByRole("button", { name: "Add email" }),
+      ).toBeInTheDocument();
+
+      expect(screen.getByRole("link", { name: "Cancel" })).toBeInTheDocument();
+
+      expect(screen.getByRole("link", { name: "Cancel" })).toHaveAttribute(
+        "href",
+        "/account/logins",
+      );
+    });
+  });
+
+  describe("Identity type : PHONE", () => {
+    it("should render proper email breadcrumbs", () => {
+      render(<AddIdentityPage type={IdentityTypes.PHONE} />);
+
+      expect(screen.getByText("Phone number | new")).toBeInTheDocument();
+    });
+
+    it("should render proper phone form elements ", () => {
+      render(<AddIdentityPage type={IdentityTypes.PHONE} />);
+
+      const addIdentityInput = screen.getByRole("textbox");
+
+      expect(addIdentityInput).toBeVisible();
+      userEvent.type(addIdentityInput, "0642424242");
+      expect(addIdentityInput).toHaveDisplayValue("0642424242");
+
+      const markAsPrimaryCheckbox = screen.getByRole("checkbox", {
+        name: "Mark this identity as my primary one",
+      });
+
+      expect(markAsPrimaryCheckbox).toBeInTheDocument();
+      expect(markAsPrimaryCheckbox).not.toBeChecked();
+      userEvent.click(markAsPrimaryCheckbox);
+      expect(markAsPrimaryCheckbox).toBeChecked();
+
+      expect(
+        screen.getByRole("button", { name: "Add phone" }),
+      ).toBeInTheDocument();
+
+      expect(screen.getByRole("link", { name: "Cancel" })).toBeInTheDocument();
+
+      expect(screen.getByRole("link", { name: "Cancel" })).toHaveAttribute(
+        "href",
+        "/account/logins",
+      );
+    });
   });
 });
