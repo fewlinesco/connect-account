f37ce72c5eec745216cc64a0b6ad2bf742cdc7f2
Oluane <rochepauline6@gmail.com>
2021-01-11 18:24:47 +0100

    Locale page test revamp with RTL (#193)

--- tests/pages/locale-page.test.tsx
+++ tests/pages/locale-page.test.tsx
@@ -1,20 +1,7 @@
-import { mount } from "enzyme";
 import React from "react";
 
-import { Input } from "@src/components/input/input";
-import { ListItem } from "@src/components/locale/locale";
-import { RadioButton } from "@src/components/radio-button/radio-button";
-import { AccountApp } from "@src/pages/_app";
+import { render, screen } from "../config/testing-library-config";
 import LocalePage from "@src/pages/account/locale";
-
-jest.mock("@src/config", () => {
-  return {
-    config: {
-      connectApplicationClientSecret: "foo-bar",
-      connectAccountSessionSalt: ".*b+x3ZXE3-h[E+Q5YC5`jr~y%CA~R-[",
-    },
-  };
-});
 
 jest.mock("@src/db-client", () => {
   return {
@@ -27,32 +14,10 @@ jest.mock("@src/db-client", () => {
 });
 
 describe("LocalePage", () => {
-  test("it shoud render an input", () => {
-    const component = mount(
-      <AccountApp>
-        <LocalePage />
-      </AccountApp>,
-    );
+  it("should render a search input", () => {
+    render(<LocalePage />);
 
-    const input = component.find(Input);
-    expect(input).toHaveLength(1);
-  });
-
-  test("it should render a list item with a checked radio button", () => {
-    const component = mount(
-      <AccountApp>
-        <LocalePage />
-      </AccountApp>,
-    );
-
-    const listItem = component.find(ListItem);
-    const radioButton = component.find(RadioButton);
-    const checkedRadioButton = component.contains(
-      <RadioButton name="locale" checked={true} />,
-    );
-
-    expect(listItem).toHaveLength(1);
-    expect(radioButton).toHaveLength(1);
-    expect(checkedRadioButton).toEqual(true);
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
+    expect(screen.getByTitle("Magnifying glass")).toBeInTheDocument();
   });
 });
