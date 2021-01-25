55f445068217ba1ba61327f0ab385d6bf29d9ffa
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-01-25 10:47:15 +0100

    Fixed delete api route

--- src/pages/api/delete-identity.ts
+++ src/pages/api/delete-identity.ts
@@ -25,20 +25,13 @@ const handler = (
 
     span.setDisclosedAttribute("Identity type", type);
 
-    if (request.method === "DELETE") {
-      return removeIdentityFromUser({ userId, type, value }).then((data) => {
-        response.statusCode = HttpStatus.ACCEPTED;
+    return removeIdentityFromUser({ userId, type, value }).then((data) => {
+      response.statusCode = HttpStatus.ACCEPTED;
 
-        response.setHeader("Content-Type", "application/json");
+      response.setHeader("Content-Type", "application/json");
 
-        response.end(JSON.stringify({ data }));
-      });
-    }
-
-    response.statusCode = HttpStatus.METHOD_NOT_ALLOWED;
-    response.end();
-
-    return Promise.reject();
+      response.end(JSON.stringify({ data }));
+    });
   });
 };
 
@@ -55,5 +48,5 @@ const wrappedHandler = wrapMiddlewares(
 );
 
 export default new Endpoint<NextApiRequest, NextApiResponse>()
-  .get(wrappedHandler)
+  .delete(wrappedHandler)
   .getHandler();
