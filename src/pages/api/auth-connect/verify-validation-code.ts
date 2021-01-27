323917fff9f5552de14ae99dbfc91b6bc753f8c9
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-01-27 21:27:40 +0100

    verify-validation-code

--- src/pages/api/auth-connect/verify-validation-code.ts
+++ src/pages/api/auth-connect/verify-validation-code.ts
@@ -1,23 +1,33 @@
 import { addIdentityToUser } from "@fewlines/connect-management";
 import { checkVerificationCode } from "@fewlines/connect-management";
 import { markIdentityAsPrimary } from "@fewlines/connect-management";
-import { HttpStatus } from "@fwl/web";
+import { Endpoint, HttpStatus } from "@fwl/web";
+import {
+  loggingMiddleware,
+  wrapMiddlewares,
+  tracingMiddleware,
+  errorMiddleware,
+  recoveryMiddleware,
+} from "@fwl/web/dist/middlewares";
+import { NextApiRequest, NextApiResponse } from "next";
 
 import { Handler } from "@src/@types/core/Handler";
 import { UserCookie } from "@src/@types/user-cookie";
 import { NoTemporaryIdentity, NoUserFound } from "@src/client-errors";
 import { removeTemporaryIdentity } from "@src/commands/remove-temporary-identity";
 import { config } from "@src/config";
+import { logger } from "@src/logger";
 import { withAuth } from "@src/middlewares/with-auth";
-import { withLogger } from "@src/middlewares/with-logger";
 import { withSentry } from "@src/middlewares/with-sentry";
-import { wrapMiddlewares } from "@src/middlewares/wrapper";
 import { getDBUserFromSub } from "@src/queries/get-db-user-from-sub";
+import getTracer from "@src/tracer";
 import { getIdentityType } from "@src/utils/get-identity-type";
 import { getServerSideCookies } from "@src/utils/server-side-cookies";
 
+const tracer = getTracer();
+
 const handler: Handler = async (request, response) => {
-  if (request.method === "POST") {
+  return tracer.span("mark-identity-as-primary handler", async (span) => {
     const { validationCode, eventId } = request.body;
 
     const userCookie = (await getServerSideCookies<UserCookie>(request, {
@@ -69,6 +79,8 @@ const handler: Handler = async (request, response) => {
 
             return response.end();
           } else if (verificationStatus === "INVALID") {
+            span.setDisclosedAttribute("Validation code invalid", true);
+
             response.statusCode = HttpStatus.BAD_REQUEST;
             response.json({ error: verificationStatus });
             return;
@@ -78,6 +90,8 @@ const handler: Handler = async (request, response) => {
             });
           }
         } else {
+          span.setDisclosedAttribute("Temporary Identity expired", true);
+
           await removeTemporaryIdentity(userCookie.sub, temporaryIdentity);
 
           response.statusCode = HttpStatus.BAD_REQUEST;
@@ -85,16 +99,28 @@ const handler: Handler = async (request, response) => {
           return;
         }
       } else {
+        span.setDisclosedAttribute("Temporary Identity not found", true);
+
         throw new NoTemporaryIdentity();
       }
     } else {
       throw new NoUserFound();
     }
-  }
-
-  response.statusCode = HttpStatus.METHOD_NOT_ALLOWED;
-
-  return Promise.reject();
+  });
 };
 
-export default wrapMiddlewares([withLogger, withSentry, withAuth], handler);
+const wrappedHandler = wrapMiddlewares(
+  [
+    tracingMiddleware(tracer),
+    recoveryMiddleware(tracer),
+    errorMiddleware(tracer),
+    loggingMiddleware(tracer, logger),
+    withSentry,
+    withAuth,
+  ],
+  handler,
+);
+
+export default new Endpoint<NextApiRequest, NextApiResponse>()
+  .post(wrappedHandler)
+  .getHandler();
