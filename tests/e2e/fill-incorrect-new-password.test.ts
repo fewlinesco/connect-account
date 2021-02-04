cdaf6761fdd0fdc40d7ec77142f91389f52941b3
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-02-04 15:20:02 +0100

    Reduced timeout of e2e tests

--- tests/e2e/fill-incorrect-new-password.test.ts
+++ tests/e2e/fill-incorrect-new-password.test.ts
@@ -14,7 +14,7 @@ import {
 import { authenticateToConnect } from "./utils/authenticate-to-connect";
 
 describe("Account Web Application update password", () => {
-  jest.setTimeout(120000);
+  jest.setTimeout(60000);
 
   beforeAll(async () => {
     await openBrowser({
