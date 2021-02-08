e0e88587ee15d6ce0a40b7bcf62877366a285b42
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-02-08 18:54:37 +0100

    Logout handler WebError

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -9,21 +9,21 @@ import {
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Handler } from "@src/@types/handler";
-import { DeleteUserCookieError } from "@src/errors";
 import { logger } from "@src/logger";
 import { withSentry } from "@src/middlewares/with-sentry";
 import getTracer from "@src/tracer";
-
-const tracer = getTracer();
+import { ERRORS_DATA, webErrorFactory } from "@src/web-errors";
 
 const handler: Handler = (_request, response): Promise<void> => {
-  return tracer.span("logout handler", async (span) => {
+  const webErrors = {
+    unexpectedError: ERRORS_DATA.UNEXPECTED_ERROR,
+  };
+
+  return getTracer().span("logout handler", async (span) => {
     await deleteServerSideCookie(response, "user-cookie").catch(() => {
       span.setDisclosedAttribute("user logged out", false);
 
-      throw new DeleteUserCookieError(
-        "Error deleting UserCookie for login out",
-      );
+      throw webErrorFactory(webErrors.unexpectedError);
     });
 
     span.setDisclosedAttribute("user logged out", true);
@@ -35,17 +35,17 @@ const handler: Handler = (_request, response): Promise<void> => {
   });
 };
 
-const wrappedHandler = wrapMiddlewares(
-  [
-    tracingMiddleware(tracer),
-    recoveryMiddleware(tracer),
-    errorMiddleware(tracer),
-    loggingMiddleware(tracer, logger),
-    withSentry,
-  ],
-  handler,
-);
-
 export default new Endpoint<NextApiRequest, NextApiResponse>()
-  .get(wrappedHandler)
+  .get(
+    wrapMiddlewares(
+      [
+        tracingMiddleware(getTracer()),
+        recoveryMiddleware(getTracer()),
+        errorMiddleware(getTracer()),
+        loggingMiddleware(getTracer(), logger),
+        withSentry,
+      ],
+      handler,
+    ),
+  )
   .getHandler();
