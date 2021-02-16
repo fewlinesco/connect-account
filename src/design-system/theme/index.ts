2456dffa70c256521e2c651c6e729c05bc2ffe85
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-02-16 09:28:01 +0100

    Revert font to system + remove décathlon theme (#235)
    * Added back system font
    
    * Removed multiple theme export
    
    * Removed theme variable
    
    * Removed google font DNS call
    
    * Fixed _document header
    
    * Removed one any from _document

--- src/design-system/theme/index.ts
+++ src/design-system/theme/index.ts
@@ -1,25 +1,2 @@
-import { DefaultTheme } from "styled-components";
-
-import {
-  decatTheme,
-  deviceBreakpoints as decatDeviceBreakpoints,
-} from "./decat-theme";
-import {
-  lightTheme,
-  deviceBreakpoints as fewlinesDeviceBreakpoints,
-  DeviceBreakpoints,
-} from "./light-theme";
-import { config } from "@src/config";
-
-let theme: DefaultTheme;
-let deviceBreakpoints: DeviceBreakpoints;
-
-if (config.connectAccountTheme === "decathlon") {
-  theme = decatTheme;
-  deviceBreakpoints = decatDeviceBreakpoints;
-} else {
-  theme = lightTheme;
-  deviceBreakpoints = fewlinesDeviceBreakpoints;
-}
-
-export { theme, deviceBreakpoints };
+export { lightTheme as theme, deviceBreakpoints } from "./light-theme";
+export type { DeviceBreakpoints } from "./light-theme";
