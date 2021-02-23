190261fb70c88b91be9237a640a7d786ba5cfd59
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-02-23 14:27:34 +0100

    Moved sentry middleware (#245)

--- src/pages/api/auth-connect/set-password.ts
+++ src/pages/api/auth-connect/set-password.ts
@@ -85,9 +85,9 @@ const wrappedHandler = wrapMiddlewares(
   [
     tracingMiddleware(getTracer()),
     recoveryMiddleware(getTracer()),
+    withSentry,
     errorMiddleware(getTracer()),
     loggingMiddleware(getTracer(), logger),
-    withSentry,
     withAuth,
   ],
   handler,
