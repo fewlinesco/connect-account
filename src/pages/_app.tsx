8db29af22b2ab566418b86122a29453f21ddc22b
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-03-16 10:10:47 +0100

    Migrated alert messages to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-import { SSRProvider } from "@react-aria/ssr";
+import { AlertMessage } from "@fwl/web";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 
+import { AlertMessages } from "@src/components/alert-message/alert-messages";
 import { GlobalStyle } from "@src/design-system/globals/global-style";
 import { theme } from "@src/design-system/theme";
 import "@src/utils/sentry";
@@ -17,20 +18,33 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
 };
 
 const AccountApp: React.FC = ({ children }) => {
+  const [alertMessages, setAlertMessages] = React.useState<AlertMessage[]>([]);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (document.cookie) {
+        setAlertMessages([
+          ...alertMessages,
+          ...JSON.parse(decodeURIComponent(document.cookie).split("=")[1]),
+        ]);
+
+        document.cookie = "alert-messages=; max-age=0; path=/;";
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <SSRProvider>
-      <ThemeProvider theme={theme}>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <title>Connect Account</title>
-        </Head>
-        <GlobalStyle />
-        {children}
-      </ThemeProvider>
-    </SSRProvider>
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Connect Account</title>
+      </Head>
+      <GlobalStyle />
+      <AlertMessages alertMessages={alertMessages} />
+      {children}
+    </ThemeProvider>
   );
 };
 
