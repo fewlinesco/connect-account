401287821e14452c154043cebecfe096c784ec82
Oluane <rochepauline6@gmail.com>
2021-03-22 22:13:19 +0100

    adding handler in sudo GSSP to get primary identities

--- src/pages/account/security/sudo.tsx
+++ src/pages/account/security/sudo.tsx
@@ -1,4 +1,12 @@
 import {
+  ConnectUnreachableError,
+  getIdentities,
+  GraphqlErrors,
+  Identity,
+  IdentityTypes,
+} from "@fewlines/connect-management";
+import { getServerSideCookies } from "@fwl/web";
+import {
   loggingMiddleware,
   tracingMiddleware,
   errorMiddleware,
@@ -9,19 +17,25 @@ import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
 import type { GetServerSideProps } from "next";
 import React from "react";
 
+import { UserCookie } from "@src/@types/user-cookie";
 import { Container } from "@src/components/containers/container";
 import { Layout } from "@src/components/page-layout";
 import { Sudo } from "@src/components/pages/sudo/sudo";
+import { config } from "@src/config";
 import { logger } from "@src/logger";
 import { authMiddleware } from "@src/middlewares/auth-middleware";
 import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
 import getTracer from "@src/tracer";
+import { ERRORS_DATA, webErrorFactory } from "@src/web-errors";
 
-const SudoPage: React.FC = () => {
+const SudoPage: React.FC<{ primaryIdentities: Identity[] }> = ({
+  primaryIdentities,
+}) => {
+  console.log(primaryIdentities);
   return (
     <Layout title="Security">
       <Container>
-        <Sudo />
+        <Sudo primaryIdentities={primaryIdentities} />
       </Container>
     </Layout>
   );
@@ -43,6 +57,61 @@ const getServerSideProps: GetServerSideProps = async (context) => {
       authMiddleware(getTracer()),
     ],
     "/account/security/sudo",
+    async (request) => {
+      const webErrors = {
+        identityNotFound: ERRORS_DATA.IDENTITY_NOT_FOUND,
+        connectUnreachable: ERRORS_DATA.CONNECT_UNREACHABLE,
+      };
+
+      const userCookie = await getServerSideCookies<UserCookie>(request, {
+        cookieName: "user-cookie",
+        isCookieSealed: true,
+        cookieSalt: config.cookieSalt,
+      });
+
+      if (userCookie) {
+        const primaryIdentities = await getIdentities(
+          config.managementCredentials,
+          userCookie.sub,
+        )
+          .then((identities) => {
+            return identities.filter((identity) => {
+              return (
+                identity.primary &&
+                (identity.type == IdentityTypes.EMAIL.toLowerCase() ||
+                  identity.type == IdentityTypes.PHONE.toLowerCase())
+              );
+            });
+          })
+          .catch((error) => {
+            if (error instanceof GraphqlErrors) {
+              throw webErrorFactory({
+                ...webErrors.identityNotFound,
+                parentError: error,
+              });
+            }
+
+            if (error instanceof ConnectUnreachableError) {
+              throw webErrorFactory({
+                ...webErrors.connectUnreachable,
+                parentError: error,
+              });
+            }
+
+            throw error;
+          });
+
+        if (primaryIdentities.length > 1) {
+          return {
+            props: {
+              primaryIdentities,
+            },
+          };
+        }
+      }
+
+      return { props: {} };
+    },
   );
 };
 
