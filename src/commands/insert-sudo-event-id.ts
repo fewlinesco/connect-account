4ba4302de67500fae937ce87626a91f63ed404d9
Oluane <rochepauline6@gmail.com>
2021-03-23 19:34:55 +0100

    adding sudo_event_ids array in dynamo user

---
+++ src/commands/insert-sudo-event-id.ts
@@ -0,0 +1,27 @@
+import { PutItemCommandOutput } from "@aws-sdk/client-dynamodb";
+
+import { putUser } from "./put-user";
+import { SudoEventId } from "@src/@types/dynamo-user";
+import { NoUserFoundError } from "@src/errors";
+import { getDBUserFromSub } from "@src/queries/get-db-user-from-sub";
+
+async function insertSudoEventId(
+  sub: string,
+  sudoEventId: SudoEventId,
+): Promise<PutItemCommandOutput> {
+  const user = await getDBUserFromSub(sub);
+
+  if (user) {
+    console.log(user);
+    const updatedUser = {
+      ...user,
+      sudo_event_ids: [...user.sudo_event_ids, sudoEventId],
+    };
+
+    return await putUser(updatedUser);
+  }
+
+  throw new NoUserFoundError();
+}
+
+export { insertSudoEventId };
