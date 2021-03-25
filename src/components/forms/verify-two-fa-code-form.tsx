c3ec46992772c92c314d709ae8a4df30f757e424
Oluane <rochepauline6@gmail.com>
2021-03-25 17:49:54 +0100

    WIP error handling

--- src/components/forms/verify-two-fa-code-form.tsx
+++ src/components/forms/verify-two-fa-code-form.tsx
@@ -4,14 +4,17 @@ import styled from "styled-components";
 import { v4 as uuidv4 } from "uuid";
 
 import { InputText } from "../input/input-text";
+import { WrongInputError } from "../input/wrong-input-error";
 import { Form } from "./form";
 import { HttpVerbs } from "@src/@types/http-verbs";
 import { Button, ButtonVariant } from "@src/components/buttons/buttons";
 import { fetchJson } from "@src/utils/fetch-json";
+import { ERRORS_DATA } from "@src/web-errors";
 
 const VerifyTwoFACodeForm: React.FC = () => {
   const [formID, setFormID] = React.useState<string>(uuidv4());
   const [verificationCode, setVerificationCode] = React.useState<string>("");
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
 
   const router = useRouter();
 
@@ -25,11 +28,26 @@ const VerifyTwoFACodeForm: React.FC = () => {
           "/api/auth-connect/verify-two-fa-validation-code",
           HttpVerbs.POST,
           { verificationCode },
-        ).then(() => {
-          router && router.push("/account/security/update");
-        });
+        )
+          .then((response) => response.json())
+          .then((parsedResponse) => {
+            console.log(parsedResponse);
+            if ("message" in parsedResponse) {
+              if (
+                parsedResponse.message === ERRORS_DATA.INVALID_BODY.message ||
+                parsedResponse.message ===
+                  ERRORS_DATA.INVALID_VALIDATION_CODE.message
+              ) {
+                setFormID(uuidv4());
+                setErrorMessage("Invalid validation code");
+              }
+            }
+
+            // router && router.push("/account/security/update");
+          });
       }}
     >
+      {errorMessage ? <WrongInputError>{errorMessage}.</WrongInputError> : null}
       <InputText
         type="text"
         name="verificationCode"
@@ -48,11 +66,6 @@ const VerifyTwoFACodeForm: React.FC = () => {
 
 const ExtendedStyledForm = styled(Form)`
   padding: ${({ theme }) => theme.spaces.xs} 0 0 0;
-
-  p {
-    padding-bottom: ${({ theme }) => theme.spaces.xs};
-    padding-left: ${({ theme }) => theme.spaces.xs};
-  }
 
   button {
     margin-bottom: 0;
