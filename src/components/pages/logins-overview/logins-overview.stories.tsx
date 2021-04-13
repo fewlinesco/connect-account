f8b68590cae1a9fa4ca225f6d84ff7807b5e9346
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-04-13 16:42:48 +0200

    Improving/refactoring logins overview for loading state texts (#447)
    * Refacto show more button
    
    * Fixed logins overview stories
    
    * Improved logins overview
    
    * Added back isLastOfTheList prop to social identity overview section
    
    * Imrpoved logins overview tests to use content object
    
    * Fixed test

--- src/components/pages/logins-overview/logins-overview.stories.tsx
+++ src/components/pages/logins-overview/logins-overview.stories.tsx
@@ -6,78 +6,80 @@ import { LoginsOverview } from "./logins-overview";
 import { SortedIdentities } from "@src/@types/sorted-identities";
 import { StoriesContainer } from "@src/components/containers/stories-container";
 
-const LoginsOverviewPage = (): JSX.Element => {
-  const sortedIdentities: SortedIdentities = {
-    phoneIdentities: [
-      {
-        id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: true,
-        status: "validated",
-        type: IdentityTypes.PHONE,
-        value: "0622116655",
-      },
-      {
-        id: "jht5dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: false,
-        status: "validated",
-        type: IdentityTypes.PHONE,
-        value: "0622116633",
-      },
-      {
-        id: "jht5dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: false,
-        status: "unvalidated",
-        type: IdentityTypes.PHONE,
-        value: "0622116622",
-      },
-    ],
-    emailIdentities: [
-      {
-        id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: true,
-        status: "validated",
-        type: IdentityTypes.EMAIL,
-        value: "test@test.test",
-      },
-      {
-        id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: false,
-        status: "validated",
-        type: IdentityTypes.EMAIL,
-        value: "test1@test.test",
-      },
-      {
-        id: "91gercc1-530b-4982-878d-33f0def6a7cf",
-        primary: false,
-        status: "unvalidated",
-        type: IdentityTypes.EMAIL,
-        value: "test2@test.test",
-      },
-    ],
-    socialIdentities: [
-      {
-        id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: true,
-        status: "validated",
-        type: IdentityTypes.GITHUB,
-        value: "",
-      },
-      {
-        id: "8u76dcc1-530b-4982-878d-33f0def6a7cf",
-        primary: false,
-        status: "validated",
-        type: IdentityTypes.FACEBOOK,
-        value: "",
-      },
-    ],
-  };
+const sortedIdentities: SortedIdentities = {
+  phoneIdentities: [
+    {
+      id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: true,
+      status: "validated",
+      type: IdentityTypes.PHONE,
+      value: "0622116655",
+    },
+    {
+      id: "jht5dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: false,
+      status: "validated",
+      type: IdentityTypes.PHONE,
+      value: "0622116633",
+    },
+    {
+      id: "jht5dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: false,
+      status: "unvalidated",
+      type: IdentityTypes.PHONE,
+      value: "0622116622",
+    },
+  ],
+  emailIdentities: [
+    {
+      id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: true,
+      status: "validated",
+      type: IdentityTypes.EMAIL,
+      value: "test@test.test",
+    },
+    {
+      id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: false,
+      status: "validated",
+      type: IdentityTypes.EMAIL,
+      value: "test1@test.test",
+    },
+    {
+      id: "91gercc1-530b-4982-878d-33f0def6a7cf",
+      primary: false,
+      status: "unvalidated",
+      type: IdentityTypes.EMAIL,
+      value: "test2@test.test",
+    },
+  ],
+  socialIdentities: [
+    {
+      id: "8f79dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: true,
+      status: "validated",
+      type: IdentityTypes.GITHUB,
+      value: "",
+    },
+    {
+      id: "8u76dcc1-530b-4982-878d-33f0def6a7cf",
+      primary: false,
+      status: "validated",
+      type: IdentityTypes.FACEBOOK,
+      value: "",
+    },
+  ],
+};
 
+const Populated = (): JSX.Element => {
   return (
     <SWRConfig
       value={{
         dedupingInterval: 0,
         fetcher: () => {
-          return { sortedIdentities };
+          return {
+            sortedIdentities,
+          };
         },
       }}
     >
@@ -88,5 +90,28 @@ const LoginsOverviewPage = (): JSX.Element => {
   );
 };
 
-export { LoginsOverviewPage };
+const Empty = (): JSX.Element => {
+  return (
+    <SWRConfig
+      value={{
+        dedupingInterval: 0,
+        fetcher: () => {
+          return {
+            sortedIdentities: {
+              phoneIdentities: [],
+              emailIdentities: [],
+              socialIdentities: [],
+            },
+          };
+        },
+      }}
+    >
+      <StoriesContainer>
+        <LoginsOverview />
+      </StoriesContainer>
+    </SWRConfig>
+  );
+};
+
+export { Populated, Empty };
 export default { title: "pages/Logins Overview", component: LoginsOverview };
