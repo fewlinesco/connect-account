fa78ac8c1faf4b43e7d3dff823c5c2434b339210
Oluane <rochepauline6@gmail.com>
2021-04-14 12:08:46 +0200

    test with fetch on client

--- src/pages/account/test.tsx
+++ src/pages/account/test.tsx
@@ -8,28 +8,46 @@ import {
 import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
 import type { GetServerSideProps } from "next";
 import React from "react";
-import useSWR from "swr";
+// import useSWR from "swr";
 
 import { Container } from "@src/components/containers/container";
 import { Layout } from "@src/components/page-layout";
 import { logger } from "@src/configs/logger";
 import getTracer from "@src/configs/tracer";
-import { SWRError } from "@src/errors/errors";
+// import { SWRError } from "@src/errors/errors";
 import { authMiddleware } from "@src/middlewares/auth-middleware";
 import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
 
-const LocalePage: React.FC = () => {
-  const { data, error } = useSWR<Record<string, string>, SWRError>(
-    "https://api.chucknorris.io/jokes/random",
-  );
+const TestPage: React.FC = () => {
+  //   const { data, error } = useSWR<Record<string, string>, SWRError>(
+  //     "https://api.chucknorris.io/jokes/random",
+  //   );
 
-  if (error) {
-    throw error;
-  }
+  //   if (error) {
+  //     throw error;
+  //   }
+
+  const [joke, setJoke] = React.useState();
+
+  React.useEffect(() => {
+    async function getJoke(): Promise<void> {
+      const result = await fetch(
+        "https://api.chucknorris.io/jokes/random",
+      ).then((data) => data.json());
+
+      console.log(result);
+
+      setJoke(result);
+    }
+
+    getJoke();
+  }, []);
 
   return (
     <Layout breadcrumbs={false} title="Test">
-      <Container>{data ? <h1>{data.value}</h1> : <h1>Nope</h1>}</Container>
+      <Container>
+        {typeof joke !== "undefined" ? <h1>{joke.value}</h1> : <h1>Nope</h1>}
+      </Container>
     </Layout>
   );
 };
@@ -68,4 +86,4 @@ const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 export { getServerSideProps };
-export default LocalePage;
+export default TestPage;
