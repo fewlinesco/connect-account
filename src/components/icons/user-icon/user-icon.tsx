e33e492817537592c7526f337a1b123c72892e4c
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-04-15 15:41:27 +0200

    Added new icons + stories

---
+++ src/components/icons/user-icon/user-icon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const UserIcon: React.FC = () => {
+  return (
+    <svg width="40" height="40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.895 17.143c-4.557 0-8.272 4.088-9.324 9.626C15.058 31.78 21.577 35.21 28.868 36c.35-1.648.56-3.692.56-6.33 0-6.923-4.276-12.527-9.533-12.527z"
+        stroke="#03083C"
+        strokeWidth="2"
+        strokeMiterlimit="10"
+      />
+      <circle cx="21.5" cy="8.5" r="4.5" stroke="#03083C" strokeWidth="2" />
+    </svg>
+  );
+};
+
+export { UserIcon };
--- src/components/pages/account-overview/account-overview.tsx
+++ src/components/pages/account-overview/account-overview.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { LoginsIcon } from "@src/components/icons/logins-icon/logins-icon";
+import { ProfileIcon } from "@src/components/icons/profile-icon/profile-icon";
 import { RightChevron } from "@src/components/icons/right-chevron/right-chevron";
 import { SecurityIcon } from "@src/components/icons/security-icon/security-icon";
 import { NeutralLink } from "@src/components/neutral-link/neutral-link";
@@ -9,6 +10,10 @@ import { SectionBox } from "@src/components/shadow-box/section-box";
 import { deviceBreakpoints } from "@src/design-system/theme";
 
 const SECTION_LIST_CONTENT = {
+  PERSONAL_INFORMATION: {
+    text: "NEED REFINEMENT",
+    icon: <ProfileIcon />,
+  },
   LOGINS: {
     text:
       "Manage your logins options, including emails, phone numbers and social logins",
