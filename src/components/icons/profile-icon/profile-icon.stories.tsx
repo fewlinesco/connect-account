4a71656a8b0addafd287f9c6479230ea976eadea
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-04-16 09:27:48 +0200

    Route the Profile integration + feature flag (#460)
    * Added new icons + stories
    
    * Fixed section name displayed
    
    * Added feature flag
    
    * Fixed configVariables
    
    * Added feature flag on section overview

---
+++ src/components/icons/profile-icon/profile-icon.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { ProfileIcon } from "./profile-icon";
+
+const StandardProfileIcon = (): JSX.Element => {
+  return <ProfileIcon />;
+};
+
+export { StandardProfileIcon };
+export default { title: "icons/Profile Icon", component: ProfileIcon };
