5b0bbd1e25e0b6231bde59b06c31d1f9314ab29c
Oluane <rochepauline6@gmail.com>
2021-04-19 16:16:49 +0200

    new routes to update profile information

---
+++ src/pages/account/profile/name/update.tsx
@@ -0,0 +1,68 @@
+import {
+  loggingMiddleware,
+  tracingMiddleware,
+  errorMiddleware,
+  recoveryMiddleware,
+  rateLimitingMiddleware,
+} from "@fwl/web/dist/middlewares";
+import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
+import type { GetServerSideProps } from "next";
+import React from "react";
+import styled from "styled-components";
+
+import { Container } from "@src/components/containers/container";
+import { Layout } from "@src/components/page-layout";
+import { configVariables } from "@src/configs/config-variables";
+import { logger } from "@src/configs/logger";
+import getTracer from "@src/configs/tracer";
+import { authMiddleware } from "@src/middlewares/auth-middleware";
+import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
+
+const UpdateNamePage: React.FC = () => {
+  return (
+    <Layout breadcrumbs={"Name | edit"} title="Personal information">
+      <Container>
+        <WIP>🏗</WIP>
+      </Container>
+    </Layout>
+  );
+};
+
+const WIP = styled.div`
+  font-size: 20rem;
+`;
+
+const getServerSideProps: GetServerSideProps = async (context) => {
+  return getServerSidePropsWithMiddlewares(
+    context,
+    [
+      tracingMiddleware(getTracer()),
+      rateLimitingMiddleware(getTracer(), logger, {
+        windowMs: 300000,
+        requestsUntilBlock: 200,
+      }),
+      recoveryMiddleware(getTracer()),
+      sentryMiddleware(getTracer()),
+      errorMiddleware(getTracer()),
+      loggingMiddleware(getTracer(), logger),
+      authMiddleware(getTracer()),
+    ],
+    "/profile/name/update",
+    () => {
+      console.log(!configVariables.featureFlag);
+      if (!configVariables.featureFlag) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+
+      return { props: {} };
+    },
+  );
+};
+
+export { getServerSideProps };
+export default UpdateNamePage;
