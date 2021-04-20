c30700c8b42b40145d8d8d3250115ce8f37bfd06
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-04-20 15:23:44 +0200

    Refactored user info overview JSX

--- src/components/pages/user-info-overview/user-info-overview.tsx
+++ src/components/pages/user-info-overview/user-info-overview.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import styled from "styled-components";
 
 import { BoxedLink } from "../logins-overview/logins-overview";
 import { Address, Profile } from "@src/@types/profile";
+import { DefaultProfilePictureIcon } from "@src/components/icons/default-profile-picture/default-profile-picture";
+import { PlusIcon } from "@src/components/icons/plus-icon/plus-icon";
 import { RightChevron } from "@src/components/icons/right-chevron/right-chevron";
+import { Separator } from "@src/components/separator/separator";
+import { SectionBox } from "@src/components/shadow-box/section-box";
 import { SkeletonTextLine } from "@src/components/skeletons/skeletons";
 
 const UserInfoOverview: React.FC<{
@@ -16,21 +21,73 @@ const UserInfoOverview: React.FC<{
   return (
     <>
       <h2>Basic information</h2>
-      <BoxedLink disableClick={false} href="#">
-        <div>
-          <p>Name</p>
-          <p>
+      <SectionBox>
+        <PictureBoxedLink disableClick={false} href="#">
+          <Flex>
             {!data ? (
-              <SkeletonTextLine fontSize={1.6} />
+              <DefaultProfilePictureIcon />
             ) : (
-              data.userInfo.profile.name
+              <UserPicture src={data.userInfo.profile.picture} alt="user" />
             )}
-          </p>
-        </div>
-        <RightChevron />
-      </BoxedLink>
+            <CategoryName>PROFILE PICTURE</CategoryName>
+          </Flex>
+          <PlusIcon />
+        </PictureBoxedLink>
+        <Separator />
+        <UserInfoSection categoryName="NAME" href="#">
+          {!data ? (
+            <SkeletonTextLine fontSize={1.6} />
+          ) : (
+            `${data.userInfo.profile.name} ${data.userInfo.profile.family_name}`
+          )}
+        </UserInfoSection>
+      </SectionBox>
     </>
   );
 };
+
+const UserInfoSection: React.FC<{
+  categoryName: string;
+  href: string;
+}> = ({ categoryName, href, children }) => {
+  return (
+    <BoxedLink disableClick={false} href={href}>
+      <CategoryContent>
+        <CategoryName>{categoryName}</CategoryName>
+        {children}
+      </CategoryContent>
+      <RightChevron />
+    </BoxedLink>
+  );
+};
+
+const Flex = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PictureBoxedLink = styled(BoxedLink)`
+  height: 8rem !important;
+`;
+
+const UserPicture = styled.img`
+  width: 6.4rem;
+  height: 6.4rem;
+  border-radius: ${({ theme }) => theme.radii[2]};
+  margin-right: 1.6rem;
+`;
+
+const CategoryName = styled.p`
+  color: ${({ theme }) => theme.colors.lightGrey};
+  font-size: ${({ theme }) => theme.fontSizes.s};
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
+  letter-spacing: 0.2rem;
+  margin-bottom: 0.7rem;
+`;
+
+const CategoryContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 export { UserInfoOverview };
