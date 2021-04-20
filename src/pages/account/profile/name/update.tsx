38768960ebf6c475027fca170f57154c4f7621a6
Pauline Roche <rochepauline6@gmail.com>
2021-04-20 16:37:01 +0200

    Front implementation of profile names update form (#464)

--- src/pages/account/profile/name/update.tsx
+++ src/pages/account/profile/name/update.tsx
@@ -8,9 +8,11 @@ import {
 import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
 import type { GetServerSideProps } from "next";
 import React from "react";
-import styled from "styled-components";
+import useSWR from "swr";
 
+import { Address, Profile } from "@src/@types/profile";
 import { Container } from "@src/components/containers/container";
+import { UpdateNamesForm } from "@src/components/forms/update-names-form";
 import { Layout } from "@src/components/page-layout";
 import { configVariables } from "@src/configs/config-variables";
 import { logger } from "@src/configs/logger";
@@ -19,18 +21,39 @@ import { authMiddleware } from "@src/middlewares/auth-middleware";
 import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
 
 const UpdateNamePage: React.FC = () => {
+  const { data, error } = useSWR<
+    {
+      userInfo: {
+        profile: Profile;
+        addresses: Address[];
+      };
+    },
+    Error
+  >("/api/profile/get-user-info");
+
+  if (error) {
+    throw error;
+  }
+
+  let namesData;
+
+  if (data) {
+    namesData = {
+      name: data.userInfo.profile.name,
+      family_name: data.userInfo.profile.family_name,
+      given_name: data.userInfo.profile.given_name,
+      middle_name: data.userInfo.profile.middle_name,
+    };
+  }
+
   return (
     <Layout breadcrumbs={"Name | edit"} title="Personal information">
       <Container>
-        <WIP>🏗</WIP>
+        <UpdateNamesForm profileNames={namesData} />
       </Container>
     </Layout>
   );
 };
-
-const WIP = styled.div`
-  font-size: 20rem;
-`;
 
 const getServerSideProps: GetServerSideProps = async (context) => {
   return getServerSidePropsWithMiddlewares(
