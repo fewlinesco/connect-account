7e2884e998dda75ff323e447ca6ba394879b6656
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-04-29 16:54:34 +0200

    Update address page

---
+++ src/pages/account/profile/address/[id]/update.tsx
@@ -0,0 +1,81 @@
+import {
+  loggingMiddleware,
+  tracingMiddleware,
+  errorMiddleware,
+  recoveryMiddleware,
+  rateLimitingMiddleware,
+} from "@fwl/web/dist/middlewares";
+import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
+import type { GetServerSideProps } from "next";
+import React from "react";
+import styled from "styled-components";
+
+import { Container } from "@src/components/containers/container";
+import { Layout } from "@src/components/page-layout";
+import { configVariables } from "@src/configs/config-variables";
+import { logger } from "@src/configs/logger";
+import getTracer from "@src/configs/tracer";
+import { authMiddleware } from "@src/middlewares/auth-middleware";
+import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
+
+const UpdateAddressPage: React.FC = () => {
+  const { data, error } = useSWR<{ identity: Identity }, Error>(
+    `/api/identity/get-identity?identityId=${identityId}`,
+  );
+
+  return (
+    <Layout breadcrumbs={false} title="Personal information">
+      <Container>
+        <WIP>🏗</WIP>
+      </Container>
+    </Layout>
+  );
+};
+
+const WIP = styled.div`
+  font-size: 20rem;
+`;
+
+const getServerSideProps: GetServerSideProps = async (context) => {
+  return getServerSidePropsWithMiddlewares(
+    context,
+    [
+      tracingMiddleware(getTracer()),
+      rateLimitingMiddleware(getTracer(), logger, {
+        windowMs: 300000,
+        requestsUntilBlock: 200,
+      }),
+      recoveryMiddleware(getTracer()),
+      sentryMiddleware(getTracer()),
+      errorMiddleware(getTracer()),
+      loggingMiddleware(getTracer(), logger),
+      authMiddleware(getTracer()),
+    ],
+    "/account/profile/address/[id]/update",
+    () => {
+      if (!configVariables.featureFlag) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+
+      if (!context?.params?.id) {
+        return {
+          notFound: true,
+        };
+      }
+
+      return {
+        props: {
+          identityId: context.params.id,
+        },
+      };
+    },
+  );
+};
+
+export { getServerSideProps };
+export default UpdateAddressPage;
