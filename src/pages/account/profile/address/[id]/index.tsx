fb226875cf95edd25dfb7219e470828d4468f2ee
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-04-29 15:03:13 +0200

    Added template page for address show

---
+++ src/pages/account/profile/address/[id]/index.tsx
@@ -0,0 +1,82 @@
+import {
+  loggingMiddleware,
+  tracingMiddleware,
+  errorMiddleware,
+  recoveryMiddleware,
+  rateLimitingMiddleware,
+} from "@fwl/web/dist/middlewares";
+import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
+import type { GetServerSideProps } from "next";
+import React from "react";
+import useSWR from "swr";
+
+import { Address } from "@src/@types/profile";
+import { Container } from "@src/components/containers/container";
+import { Layout } from "@src/components/page-layout";
+import { configVariables } from "@src/configs/config-variables";
+import { logger } from "@src/configs/logger";
+import getTracer from "@src/configs/tracer";
+import { authMiddleware } from "@src/middlewares/auth-middleware";
+import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
+
+const AddressOverviewPage: React.FC<{ addressID: string }> = ({
+  addressID,
+}) => {
+  const { data, error } = useSWR<{ address: Address }, Error>(
+    `/api/profile/get-address?identityId=${addressID}`,
+  );
+
+  if (error) {
+    throw error;
+  }
+
+  return (
+    <Layout breadcrumbs={false} title="Personal information">
+      <Container></Container>
+    </Layout>
+  );
+};
+
+const getServerSideProps: GetServerSideProps = async (context) => {
+  return getServerSidePropsWithMiddlewares(
+    context,
+    [
+      tracingMiddleware(getTracer()),
+      rateLimitingMiddleware(getTracer(), logger, {
+        windowMs: 300000,
+        requestsUntilBlock: 200,
+      }),
+      recoveryMiddleware(getTracer()),
+      sentryMiddleware(getTracer()),
+      errorMiddleware(getTracer()),
+      loggingMiddleware(getTracer(), logger),
+      authMiddleware(getTracer()),
+    ],
+    "account/profile/address/[id]",
+    () => {
+      if (!configVariables.featureFlag) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+
+      if (!context?.params?.id) {
+        return {
+          notFound: true,
+        };
+      }
+
+      return {
+        props: {
+          identityId: context.params.id,
+        },
+      };
+    },
+  );
+};
+
+export { getServerSideProps };
+export default AddressOverviewPage;
