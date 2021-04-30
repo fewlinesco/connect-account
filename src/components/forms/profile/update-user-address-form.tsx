51dc54ca46c50c4b309f7a58237212bcc1005fb4
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-04-30 11:11:00 +0200

    Added last inputs

--- src/components/forms/profile/update-user-address-form.tsx
+++ src/components/forms/profile/update-user-address-form.tsx
@@ -9,31 +9,34 @@ import { Button, ButtonVariant } from "@src/components/buttons/buttons";
 import { FakeButton } from "@src/components/buttons/fake-button";
 import { NeutralLink } from "@src/components/neutral-link/neutral-link";
 
-const UpdateUserAddressForm: React.FC<{ userAddress?: Address }> = ({
-  userAddress,
-}) => {
+const UpdateUserAddressForm: React.FC<{
+  userAddress?: Address;
+}> = ({ userAddress }) => {
   const [formID, setFormID] = React.useState<string>(uuidv4());
-  // const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
 
-  const [
-    updatedStreetAddress,
-    setUpdatedStreetAddress,
-  ] = React.useState<string>("");
-  const [
-    updatedStreetAddress2,
-    setUpdatedStreetAddress2,
-  ] = React.useState<string>("");
+  const [updatedAddress, setUpdatedAddress] = React.useState<Address>({
+    id: "",
+    sub: "",
+    street_address: "string",
+    locality: "string",
+    region: "string",
+    postal_code: "string",
+    country: "string",
+    kind: "string",
+    created_at: "string",
+    updated_at: "string",
+    street_address_2: "string",
+    primary: false,
+  });
 
   React.useEffect(() => {
     if (userAddress) {
-      setUpdatedStreetAddress(userAddress.street_address);
-      setUpdatedStreetAddress2(userAddress.street_address_2);
+      setUpdatedAddress(userAddress);
     }
   }, [userAddress]);
 
   return (
     <>
-      {/* {errorMessage ? <WrongInputError>{errorMessage}.</WrongInputError> : null} */}
       <Form
         formID={formID}
         onSubmit={async () => {
@@ -43,23 +46,81 @@ const UpdateUserAddressForm: React.FC<{ userAddress?: Address }> = ({
       >
         <InputText
           type="text"
-          name="name"
+          name="street-address"
           placeholder="Enter your street address"
-          value={updatedStreetAddress}
+          value={updatedAddress.street_address}
           onChange={(value) => {
-            setUpdatedStreetAddress(value);
+            setUpdatedAddress({
+              ...updatedAddress,
+              street_address: value,
+            });
           }}
-          label="Name *"
+          label="Street address *"
         />
         <InputText
           type="text"
-          name="name"
+          name="street-address2"
           placeholder="Enter your complementary street address"
-          value={updatedStreetAddress2}
+          value={updatedAddress.street_address_2}
           onChange={(value) => {
-            setUpdatedStreetAddress2(value);
+            setUpdatedAddress({
+              ...updatedAddress,
+              street_address_2: value,
+            });
           }}
-          label="Name *"
+          label="Complementary street address"
+        />
+        <InputText
+          type="text"
+          name="locality"
+          placeholder="Enter your locality"
+          value={updatedAddress.locality}
+          onChange={(value) => {
+            setUpdatedAddress({
+              ...updatedAddress,
+              locality: value,
+            });
+          }}
+          label="Locality *"
+        />
+        <InputText
+          type="text"
+          name="locality"
+          placeholder="Enter your region"
+          value={updatedAddress.region}
+          onChange={(value) => {
+            setUpdatedAddress({
+              ...updatedAddress,
+              region: value,
+            });
+          }}
+          label="Region *"
+        />
+        <InputText
+          type="text"
+          name="locality"
+          placeholder="Enter your postal code"
+          value={updatedAddress.postal_code}
+          onChange={(value) => {
+            setUpdatedAddress({
+              ...updatedAddress,
+              postal_code: value,
+            });
+          }}
+          label="Postal code *"
+        />
+        <InputText
+          type="text"
+          name="locality"
+          placeholder="Enter your country"
+          value={updatedAddress.country}
+          onChange={(value) => {
+            setUpdatedAddress({
+              ...updatedAddress,
+              country: value,
+            });
+          }}
+          label="Country *"
         />
         <Button type="submit" variant={ButtonVariant.PRIMARY}>
           Update my address
