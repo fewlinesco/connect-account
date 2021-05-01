675d8cb9bf02986a990f82c39b45f0ffb1e32a81
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-05-01 11:19:45 +0200

    Added sentry SDK

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { withSentryConfig } = require("@sentry/nextjs");
+
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const moduleExports = {
   webpack(config, options) {
     const { isServer } = options;
 
@@ -26,3 +34,20 @@ module.exports = {
     return config;
   },
 };
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+  project: "Connect.Account",
+  url:
+    "https://6baf0213093a428d9e9ec7ae0979868c@o287463.ingest.sentry.io/5387583",
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
