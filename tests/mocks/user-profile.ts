72c740c20ec61cd33734af9b0879472ea592a03f
Oluane <rochepauline6@gmail.com>
2021-05-18 16:16:52 +0200

    adding integration tests for add & update user profile

---
+++ tests/mocks/user-profile.ts
@@ -0,0 +1,21 @@
+import { Profile } from "@src/@types/profile";
+
+const userProfile: Profile = {
+  sub: "edc29cc7-35d5-4409-850a-dab5e19beb58",
+  name: "Luz",
+  family_name: "Reinger",
+  given_name: "Sim",
+  middle_name: "Nikolas",
+  nickname: "Kaleb",
+  preferred_username: "Jovani",
+  profile: "https://danielle.biz",
+  picture: "//placeimg.com/640/480",
+  website: "https://eileen.biz",
+  gender: "female",
+  birthdate: "2020-10-19",
+  zoneinfo: "FAKE/zone_info",
+  locale: "ro",
+  updated_at: "2021-04-15T07:30:49.924Z",
+};
+
+export { userProfile };
