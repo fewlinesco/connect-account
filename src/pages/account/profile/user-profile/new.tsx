20dc8a93569f63e2b5ef44ce8a04f4e206c31a8b
Oluane <rochepauline6@gmail.com>
2021-05-18 15:46:02 +0200

    new front page for new user profile

---
+++ src/pages/account/profile/user-profile/new.tsx
@@ -0,0 +1,64 @@
+import {
+  loggingMiddleware,
+  tracingMiddleware,
+  errorMiddleware,
+  recoveryMiddleware,
+  rateLimitingMiddleware,
+} from "@fwl/web/dist/middlewares";
+import { getServerSidePropsWithMiddlewares } from "@fwl/web/dist/next";
+import React from "react";
+
+import { Container } from "@src/components/containers/container";
+import { UserProfileForm } from "@src/components/forms/profile/user-profile-form";
+import { Layout } from "@src/components/page-layout";
+import { configVariables } from "@src/configs/config-variables";
+import { logger } from "@src/configs/logger";
+import getTracer from "@src/configs/tracer";
+import { authMiddleware } from "@src/middlewares/auth-middleware";
+import { sentryMiddleware } from "@src/middlewares/sentry-middleware";
+
+import type { GetServerSideProps } from "next";
+
+const NewUserProfilePage: React.FC = () => {
+  return (
+    <Layout breadcrumbs={"Profile | new"} title="Personal information">
+      <Container>
+        <UserProfileForm isCreation={true} />
+      </Container>
+    </Layout>
+  );
+};
+
+const getServerSideProps: GetServerSideProps = async (context) => {
+  return getServerSidePropsWithMiddlewares(
+    context,
+    [
+      tracingMiddleware(getTracer()),
+      rateLimitingMiddleware(getTracer(), logger, {
+        windowMs: 300000,
+        requestsUntilBlock: 200,
+      }),
+      recoveryMiddleware(getTracer()),
+      sentryMiddleware(getTracer()),
+      errorMiddleware(getTracer()),
+      loggingMiddleware(getTracer(), logger),
+      authMiddleware(getTracer()),
+    ],
+    "/profile/user-profile/new",
+    () => {
+      if (!configVariables.featureFlag) {
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+
+      return { props: {} };
+    },
+  );
+};
+
+export { getServerSideProps };
+export default NewUserProfilePage;
