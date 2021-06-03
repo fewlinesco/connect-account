604da5122833f48cf92be1b09891e5c067280f1f
Oluane <rochepauline6@gmail.com>
2021-06-03 15:49:25 +0200

    WIP test

---
+++ tests/e2e/profile-happy-path.test.ts
@@ -0,0 +1,87 @@
+import {
+  closeBrowser,
+  openBrowser,
+  text,
+  click,
+  textBox,
+  into,
+  write,
+  clear,
+  link,
+} from "taiko";
+
+import { authenticateToConnect } from "./utils/authenticate-to-connect";
+
+describe("Profile Happy path", () => {
+  jest.setTimeout(60000);
+
+  beforeAll(async () => {
+    await openBrowser({
+      args: [
+        "--window-size=1440,1000",
+        "--no-sandbox",
+        "--start-maximized",
+        "--disable-dev-shm",
+      ],
+      headless: false,
+      observe: false,
+      observeTime: 2000,
+    });
+  });
+
+  afterAll(async () => {
+    await closeBrowser();
+  });
+
+  test("it should redirect", async (done) => {
+    expect.assertions(10);
+
+    try {
+      await authenticateToConnect();
+
+      expect(await text("Personal Information").exists()).toBeTruthy();
+      await click("Personal Information");
+
+      expect(
+        await text("Update your personal information").exists(),
+      ).toBeTruthy();
+      await click("Update your personal information");
+
+      expect(await text("Username").exists()).toBeTruthy();
+
+      await clear(textBox({ value: "Clark" }));
+      await write(
+        "Superman",
+        into(textBox({ placeholder: "Enter your username" })),
+      );
+      expect(await textBox({ value: "Superman" }).exists()).toBeTruthy();
+
+      await click("Update my information");
+      expect(await text("Your profile has been updated").exists()).toBeTruthy();
+
+      expect(await link("Add new address").exists()).toBeTruthy();
+      await click(link("Add new address"));
+
+      expect(await text("Address | new").exists()).toBeTruthy();
+
+      await write(
+        "Metropolis",
+        into(textBox({ placeholder: "Enter your locality" })),
+      );
+
+      await write(
+        "59000",
+        into(textBox({ placeholder: "Enter your postal code" })),
+      );
+
+      await write(
+        "United States of America",
+        into(textBox({ placeholder: "Enter your country" })),
+      );
+
+      done();
+    } catch (error) {
+      done(error);
+    }
+  });
+});
