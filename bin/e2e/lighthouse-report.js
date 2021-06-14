7f4e12fd644dc97d995e28c62d356f70efcea04c
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-06-14 16:06:27 +0200

    Added lighthouse script

---
+++ bin/e2e/lighthouse-report.js
@@ -0,0 +1,79 @@
+class LighthouseReport {
+  getUrls() {
+    if (process.env.CONNECT_TEST_ACCOUNT_URL === undefined) {
+      throw new Error(
+        "CONNECT_TEST_ACCOUNT_URL environment variable is undefined",
+      );
+    }
+
+    const reviewAppURL = process.env.CONNECT_TEST_ACCOUNT_URL;
+
+    return [reviewAppURL];
+  }
+
+  /* eslint-disable no-async-promise-executor */
+  connect(browser) {
+    if (process.env.CONNECT_TEST_ACCOUNT_URL === undefined) {
+      throw new Error(
+        "CONNECT_TEST_ACCOUNT_URL environment variable is undefined",
+      );
+    }
+
+    if (process.env.CONNECT_TEST_ACCOUNT_EMAIL === undefined) {
+      throw new Error(
+        "CONNECT_TEST_ACCOUNT_EMAIL environment variable is undefined",
+      );
+    }
+
+    if (process.env.CONNECT_TEST_ACCOUNT_PASSWORD === undefined) {
+      throw new Error(
+        "CONNECT_TEST_ACCOUNT_PASSWORD environment variable is undefined",
+      );
+    }
+
+    return new Promise(async (resolve, _reject) => {
+      const reviewAppURL = process.env.CONNECT_TEST_ACCOUNT_URL;
+
+      const page = await browser.newPage();
+      console.log("Page open");
+      await page.goto(reviewAppURL);
+      console.log("App loaded");
+
+      await page.click("a");
+      console.log("Login button clicked");
+      await page.waitForTimeout(5000);
+
+      await page.waitForSelector("input[type=email]", {
+        visible: true,
+        timeout: 0,
+      });
+      await page.type(
+        "input[type=email]",
+        process.env.CONNECT_TEST_ACCOUNT_EMAIL,
+        {
+          delay: 50,
+        },
+      );
+      await page.click("[type=submit]");
+      await page.waitForTimeout(5000);
+
+      await page.waitForSelector("input[type=password]", {
+        visible: true,
+        timeout: 0,
+      });
+      await page.type(
+        "input[type=password]",
+        process.env.CONNECT_TEST_ACCOUNT_PASSWORD,
+        {
+          delay: 50,
+        },
+      );
+      await page.click("[type=submit]");
+      await page.waitForTimeout(10000);
+
+      resolve(browser);
+    });
+  }
+}
+
+module.exports = new LighthouseReport();
