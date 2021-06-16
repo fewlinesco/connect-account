421647492f30da94ebe39b13719c28d2f8311c45
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-06-16 11:52:52 +0200

    Bump next 11 (#697)
    * Updated next config for webpack 5
    
    * Updated to next 11
    
    * Fixed alert messages hook dependencies
    
    * Fixed lint warnings

--- src/configs/rate-limiting-config.ts
+++ src/configs/rate-limiting-config.ts
@@ -2,9 +2,9 @@ import memjs from "memjs";
 
 const memcachedUrl = `${process.env.MEMCACHED_CLIENT_USERNAME}:${process.env.MEMCACHED_CLIENT_PASSWORD}@${process.env.MEMCACHED_CLIENT_SERVERS}`;
 const memcachedClient = memjs.Client.create(memcachedUrl);
-
-export default {
+const rateLimitingConfig = {
   windowMs: 300000,
   requestsUntilBlock: 200,
   memcachedClient,
 };
+export default rateLimitingConfig;
