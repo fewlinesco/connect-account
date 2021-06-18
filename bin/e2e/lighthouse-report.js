bc2e2677f006b6fbbf649664c85f97c7c752148c
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-06-18 12:06:30 +0200

    Refacto

--- bin/e2e/lighthouse-report.js
+++ bin/e2e/lighthouse-report.js
@@ -1,6 +1,6 @@
 let counter = 0;
 
-async function login(page, context) {
+async function login(page) {
   if (process.env.CONNECT_TEST_ACCOUNT_URL === undefined) {
     throw new Error(
       "CONNECT_TEST_ACCOUNT_URL environment variable is undefined",
@@ -19,26 +19,25 @@ async function login(page, context) {
     );
   }
 
-  console.log("Page open");
+  console.log("Page opened.");
   await page.goto(process.env.CONNECT_TEST_ACCOUNT_URL);
-  console.log("App loaded");
+  console.log("App loaded.");
 
   await page.click("a");
-  console.log("Login button clicked");
+  console.log("Login button clicked.");
   await page.waitForTimeout(5000);
 
   await page.waitForSelector("input[type=email]", {
     visible: true,
     timeout: 0,
   });
-  console.log("Connect URL", await page.url());
   await page.type("input[type=email]", process.env.CONNECT_TEST_ACCOUNT_EMAIL, {
     delay: 50,
   });
   await page.click("[type=submit]");
+  console.log("Email submitted.");
   await page.waitForTimeout(5000);
-  console.log("Email submitted");
-  console.log("Connect URL", await page.url());
+
   await page.waitForSelector("input[type=password]", {
     visible: true,
     timeout: 0,
@@ -51,12 +50,9 @@ async function login(page, context) {
     },
   );
   await page.click("[type=submit]");
-  console.log("Password submitted");
+  console.log("Password submitted.");
 
-  await page.waitForNavigation();
-
-  await page.goto(context.url);
-  console.log("FLAG", await page.url());
+  await page.waitForTimeout(5000);
 }
 
 async function setup(browser, context) {
@@ -66,7 +62,7 @@ async function setup(browser, context) {
   if (counter === 0) {
     await page.goto(context.url);
   } else {
-    await login(page, context);
+    await login(page);
   }
 
   await page.close();
