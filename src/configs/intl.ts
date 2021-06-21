95e1460fba7aad924190c2da8ab40b5283f807be
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-06-21 13:32:36 +0200

    Added getLocaleFromRequest

--- src/configs/intl.ts
+++ src/configs/intl.ts
@@ -1,4 +1,6 @@
 import { createIntl, createIntlCache } from "@formatjs/intl";
+import { Span } from "@fwl/tracing";
+import { NextApiRequest } from "next";
 
 import * as locales from "@content/locales";
 
@@ -18,12 +20,28 @@ const frIntl = createIntl(
   cache,
 );
 
-function formatAlertMessage(locale: "en" | "fr", id: string): string {
-  if (locale === "fr") {
-    return frIntl.formatMessage({ id });
+function formatAlertMessage(locale: string, id: string): string {
+  switch (locale) {
+    case "fr":
+      return frIntl.formatMessage({ id });
+    case "en":
+      return enIntl.formatMessage({ id });
+    default:
+      throw new Error(
+        `${locale} locale not allowed. Please choose one of the following:\nen\nfr`,
+      );
   }
-
-  return enIntl.formatMessage({ id });
 }
 
-export { formatAlertMessage };
+function getLocaleFromRequest(request: NextApiRequest, span: Span): string {
+  let locale = request.cookies["NEXT_LOCALE"] || "en";
+
+  if (locale[0] === '"') {
+    locale = locale.slice(1, -1);
+  }
+
+  span.setDisclosedAttribute("locale", locale);
+  return locale;
+}
+
+export { formatAlertMessage, getLocaleFromRequest };
