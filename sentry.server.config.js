b18eab54bb24b43edac2227e792f645d01ab28ea
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-07-06 12:19:21 +0200

    Improved defaults config files

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,14 +2,22 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://6baf0213093a428d9e9ec7ae0979868c@o287463.ingest.sentry.io/5387583',
+  dsn:
+    SENTRY_DSN ||
+    "https://6baf0213093a428d9e9ec7ae0979868c@o287463.ingest.sentry.io/5387583",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  environment:
+    process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || process.env.NODE_ENV,
+  enabled:
+    process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === "development"
+      ? false
+      : process.env.NODE_ENV === "production",
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
