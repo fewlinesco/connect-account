14de1632173d60f64a598c96f57dbd7f5bd2818f
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-07-07 12:58:39 +0200

    Added lh script + cci workflow

---
+++ bin/lighthouse-report.js
@@ -0,0 +1,72 @@
+let counter = 0;
+
+async function login(page) {
+  if (process.env.CONNECT_TEST_ACCOUNT_URL === undefined) {
+    throw new Error(
+      "CONNECT_TEST_ACCOUNT_URL environment variable is undefined",
+    );
+  }
+
+  if (process.env.CONNECT_TEST_ACCOUNT_EMAIL === undefined) {
+    throw new Error(
+      "CONNECT_TEST_ACCOUNT_EMAIL environment variable is undefined",
+    );
+  }
+
+  if (process.env.CONNECT_TEST_ACCOUNT_PASSWORD === undefined) {
+    throw new Error(
+      "CONNECT_TEST_ACCOUNT_PASSWORD environment variable is undefined",
+    );
+  }
+
+  console.log("Page opened.");
+  await page.goto(process.env.CONNECT_TEST_ACCOUNT_URL);
+  console.log("App loaded.");
+
+  await page.click("a");
+  console.log("Login button clicked.");
+  await page.waitForTimeout(5000);
+
+  await page.waitForSelector("input[type=email]", {
+    visible: true,
+    timeout: 0,
+  });
+  await page.type("input[type=email]", process.env.CONNECT_TEST_ACCOUNT_EMAIL, {
+    delay: 50,
+  });
+  await page.click("[type=submit]");
+  console.log("Email submitted.");
+  await page.waitForTimeout(5000);
+
+  await page.waitForSelector("input[type=password]", {
+    visible: true,
+    timeout: 0,
+  });
+  await page.type(
+    "input[type=password]",
+    process.env.CONNECT_TEST_ACCOUNT_PASSWORD,
+    {
+      delay: 50,
+    },
+  );
+  await page.click("[type=submit]");
+  console.log("Password submitted.");
+
+  await page.waitForTimeout(5000);
+}
+
+async function setup(browser, context) {
+  const page = await browser.newPage();
+  await page.setCacheEnabled(true);
+
+  if (counter === 0) {
+    await page.goto(context.url);
+  } else {
+    await login(page);
+  }
+
+  await page.close();
+  counter++;
+}
+
+module.exports = setup;
---
+++ lighthouserc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  ci: {
+    collect: {
+      puppeteerScript: "./bin/lighthouse-report.js",
+      puppeteerLaunchOptions: {
+        args: [
+          "--allow-no-sandbox-job",
+          "--allow-sandbox-debugging",
+          "--no-sandbox",
+          "--disable-gpu",
+          "--disable-gpu-sandbox",
+          "--display",
+        ],
+      },
+      numberOfRuns: 1,
+      disableStorageReset: true,
+      settings: {
+        disableStorageReset: true,
+        maxWaitForLoad: 60000,
+        throttlingMethod: "devtools",
+      },
+      url: [
+        process.env.CONNECT_TEST_ACCOUNT_URL,
+        `${process.env.CONNECT_TEST_ACCOUNT_URL}/account/logins`,
+        `${process.env.CONNECT_TEST_ACCOUNT_URL}/account/profile`,
+      ],
+    },
+    upload: {
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.5 }],
+        "categories:accessibility": ["error", { minScore: 0.5 }],
+        "categories:best-practices": ["error", { minScore: 0.5 }],
+        "categories:seo": ["error", { minScore: 0.5 }],
+      },
+    },
+  },
+};
