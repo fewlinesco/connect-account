87c71426dd28d6e26c77ad1ba038ce0b0575248d
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-07-19 13:56:01 +0200

    Cleaned up config files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withSentryConfig } = require("@sentry/nextjs");
 
@@ -39,12 +35,6 @@ const moduleExports = {
   },
 };
 const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -6,7 +6,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://6baf0213093a428d9e9ec7ae0979868c@o287463.ingest.sentry.io/5387583",
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   environment:
     process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || process.env.NODE_ENV,
@@ -14,8 +13,4 @@ Sentry.init({
     process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === "development"
       ? false
       : process.env.NODE_ENV === "production",
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
 });
