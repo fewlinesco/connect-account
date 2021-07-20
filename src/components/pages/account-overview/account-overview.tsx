ee4386741659b5fd3f3a645b0586747582b3a884
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-07-20 18:33:38 +0200

    Added loading state component in overview nav

--- src/components/pages/account-overview/account-overview.tsx
+++ src/components/pages/account-overview/account-overview.tsx
@@ -9,6 +9,7 @@ import { RightChevron } from "@src/components/icons/right-chevron/right-chevron"
 import { getSectionListContent } from "@src/components/navigation-bars/navigation-sections";
 import { NeutralLink } from "@src/components/neutral-link/neutral-link";
 import { SectionBox } from "@src/components/shadow-box/section-box";
+import { SkeletonTextLine } from "@src/components/skeletons/skeletons";
 import { deviceBreakpoints } from "@src/design-system/theme";
 import { SWRError } from "@src/errors/errors";
 import { getNavSectionHref } from "@src/utils/getNavSectionHref";
@@ -44,7 +45,22 @@ const AccountOverview: React.FC = () => {
   return (
     <>
       {getSectionListContent(userProfile ? false : true).map(
-        ([sectionName, { textID, icon }]) => {
+        ([sectionName, { textID, icon }], index) => {
+          if (index === 0 && !userProfile) {
+            return (
+              <SectionBox key={sectionName}>
+                <SectionLink href={getNavSectionHref(sectionName)}>
+                  {icon}
+                  <TextBox>
+                    <SkeletonTextLine fontSize={1.6} width={50} />
+                    <SkeletonTextLine fontSize={1.6} width={100} />
+                  </TextBox>
+                  <RightChevron />
+                </SectionLink>
+              </SectionBox>
+            );
+          }
+
           return (
             <SectionBox key={sectionName}>
               <SectionLink href={getNavSectionHref(sectionName)}>
@@ -77,6 +93,10 @@ const TextBox = styled.div`
   flex-grow: 2;
   font-size: ${({ theme }) => theme.fontSizes.xs};
   line-height: ${({ theme }) => theme.lineHeights.title};
+
+  div {
+    margin: 1.5rem 0;
+  }
 
   @media ${deviceBreakpoints.m} {
     max-width: 50%;
