fcc3fc1d8ddd8556bfcca5687e99e5ad281f670a
Clement <meyclem@gmail.com>
2021-07-21 13:48:55 +0200

    Use swr mutate for addresses (#832)
    ## Description
    
    This PR adds the mutation of SWR's cache when updating or creating an address.
    I did the same for "mark as primary" and "delete" actions.
    
    ## Issues
    
    [CU-adzn4p](https://app.clickup.com/t/adzn4p)

--- src/components/pages/address-overview/address-overview.tsx
+++ src/components/pages/address-overview/address-overview.tsx
@@ -1,7 +1,9 @@
+import { HttpStatus } from "@fwl/web";
 import { useRouter } from "next/router";
 import React from "react";
 import { useIntl } from "react-intl";
 import styled from "styled-components";
+import useSWR, { mutate } from "swr";
 
 import { Address } from "@src/@types/profile";
 import { PrimaryBadge } from "@src/components/badges/badges";
@@ -11,6 +13,7 @@ import { FakeButton } from "@src/components/buttons/fake-button";
 import { ConfirmationBox } from "@src/components/confirmation-box/confirmation-box";
 import { NeutralLink } from "@src/components/neutral-link/neutral-link";
 import { SkeletonTextLine } from "@src/components/skeletons/skeletons";
+import { SWRError } from "@src/errors/errors";
 import { fetchJson } from "@src/utils/fetch-json";
 import {
   capitalizeFirstLetter,
@@ -26,6 +29,31 @@ const AddressOverview: React.FC<{ address?: Address }> = ({ address }) => {
   const [deleteConfirmationBoxOpen, setDeleteConfirmationBoxOpen] =
     React.useState<boolean>(false);
   const [preventAnimation, setPreventAnimation] = React.useState<boolean>(true);
+
+  const { data: addresses } = useSWR<Address[], SWRError>(
+    "/api/profile/addresses",
+    async (url) => {
+      return await fetch(url).then(async (response) => {
+        if (!response.ok) {
+          const error = new SWRError(
+            "An error occurred while fetching the data.",
+          );
+
+          if (response.status === HttpStatus.NOT_FOUND) {
+            error.info = await response.json();
+            error.statusCode = response.status;
+            return;
+          }
+
+          error.info = await response.json();
+          error.statusCode = response.status;
+          throw error;
+        }
+
+        return response.json();
+      });
+    },
+  );
 
   return (
     <>
@@ -86,7 +114,34 @@ const AddressOverview: React.FC<{ address?: Address }> = ({ address }) => {
                       "POST",
                       {},
                     )
-                      .then(() => router && router.push("/account/profile"))
+                      .then(() => {
+                        const modifiedAddresses = [
+                          ...(addresses || ([] as Address[])),
+                        ];
+                        const oldMarkedAddress = modifiedAddresses.find(
+                          (address) => address.primary,
+                        );
+                        modifiedAddresses.forEach(
+                          (address) => (address.primary = false),
+                        );
+                        const addressToMark = modifiedAddresses.find(
+                          (a) => a.id === address.id,
+                        );
+                        if (addressToMark && oldMarkedAddress) {
+                          addressToMark.primary = true;
+                          mutate("/api/profile/addresses", modifiedAddresses);
+                          mutate(
+                            `/api/profile/addresses/${oldMarkedAddress.id}`,
+                            oldMarkedAddress,
+                          );
+                          mutate(
+                            `/api/profile/addresses/${addressToMark.id}`,
+                            oldMarkedAddress,
+                          );
+                        }
+
+                        router && router.push("/account/profile");
+                      })
                       .catch((error) => {
                         throw error;
                       });
@@ -130,7 +185,20 @@ const AddressOverview: React.FC<{ address?: Address }> = ({ address }) => {
             variant={ButtonVariant.PRIMARY}
             onPress={() => {
               fetchJson(`/api/profile/addresses/${address?.id}`, "DELETE", {})
-                .then(() => router && router.push("/account/profile"))
+                .then(async () => {
+                  if (addresses && address) {
+                    const addressToDelete = addresses.find(
+                      (a) => a.id === address.id,
+                    );
+                    if (addressToDelete) {
+                      const index = addresses.indexOf(addressToDelete);
+                      const modifiedAddresses = [...addresses];
+                      modifiedAddresses.splice(index, 1);
+                      await mutate("/api/profile/addresses", modifiedAddresses);
+                    }
+                  }
+                  router && router.push("/account/profile");
+                })
                 .catch((error) => {
                   throw error;
                 });
