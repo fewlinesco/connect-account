b883e099d6ec84702a5a9e22f4cf9d9c4286c990
Anthony Montaigne <31956107+Fuzznimp@users.noreply.github.com>
2021-07-22 19:41:09 +0200

    Lighthouse report in CI (#790)
    * Added lh script + cci workflow
    
    * Added incognito flag
    
    * Trying new config
    
    * Added env var in cci config
    
    * Removed incognito
    
    * Added screenshots
    
    * Added try catch
    
    * Updated staging URL
    
    * Added user profile lh audit
    
    * Lowered audit metrics
    
    * Added review app URL for lh audits
    
    * Added lh reports on staging
    
    * PR amends

---
+++ bin/lighthouse-report.js
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+
+let COUNTER = 0;
+const SCREENSHOTS_DIR_PATH = "/home/circleci/project/screenshots";
+
+function getTargetedPath(context) {
+  return `${SCREENSHOTS_DIR_PATH}/#${COUNTER}_${context}.png`;
+}
+
+async function login(page) {
+  if (process.env.CONNECT_TEST_ACCOUNT_URL === undefined) {
+    throw new Error(
+      "CONNECT_TEST_ACCOUNT_URL environment variable is undefined",
+    );
+  }
+
+  if (process.env.CONNECT_TEST_ACCOUNT_EMAIL === undefined) {
+    throw new Error(
+      "CONNECT_TEST_ACCOUNT_EMAIL environment variable is undefined",
+    );
+  }
+
+  if (process.env.CONNECT_TEST_ACCOUNT_PASSWORD === undefined) {
+    throw new Error(
+      "CONNECT_TEST_ACCOUNT_PASSWORD environment variable is undefined",
+    );
+  }
+
+  try {
+    console.log("Page opened.");
+    await page.goto(process.env.CONNECT_TEST_ACCOUNT_URL);
+    console.log("App loaded.");
+
+    await page.click("a");
+    console.log("Login button clicked.");
+    await page.waitForTimeout(5000);
+
+    await page.waitForSelector("input[type=email]", {
+      visible: true,
+      timeout: 0,
+    });
+    await page.type(
+      "input[type=email]",
+      process.env.CONNECT_TEST_ACCOUNT_EMAIL,
+      {
+        delay: 50,
+      },
+    );
+    await page.click("[type=submit]");
+    console.log("Email submitted.");
+    await page.waitForTimeout(5000);
+
+    await page.waitForSelector("input[type=password]", {
+      visible: true,
+      timeout: 0,
+    });
+    await page.type(
+      "input[type=password]",
+      process.env.CONNECT_TEST_ACCOUNT_PASSWORD,
+      {
+        delay: 50,
+      },
+    );
+
+    if ((await page.$("[type=submit]")) !== null) {
+      await page.click("[type=submit]");
+    }
+
+    return page.waitForTimeout(5000);
+  } catch (error) {
+    await page.screenshot({
+      path: getTargetedPath("login"),
+    });
+
+    throw error;
+  }
+}
+
+async function setup(browser, context) {
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1440, height: 1000 });
+  await page.setCacheEnabled(true);
+
+  try {
+    if (!fs.existsSync(SCREENSHOTS_DIR_PATH)) {
+      fs.mkdirSync(SCREENSHOTS_DIR_PATH);
+    }
+
+    if (COUNTER !== 1) {
+      await page.goto(context.url);
+    } else {
+      await login(page);
+    }
+
+    COUNTER++;
+    return page.close();
+  } catch (error) {
+    await page.screenshot({
+      path: getTargetedPath("setup"),
+    });
+
+    throw error;
+  }
+}
+
+module.exports = setup;
---
+++ lighthouserc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  ci: {
+    collect: {
+      puppeteerScript: "./bin/lighthouse-report.js",
+      puppeteerLaunchOptions: {
+        args: [
+          "--allow-no-sandbox-job",
+          "--allow-sandbox-debugging",
+          "--no-sandbox",
+          "--disable-gpu",
+          "--disable-gpu-sandbox",
+          "--display",
+        ],
+      },
+      numberOfRuns: 1,
+      settings: {
+        maxWaitForLoad: 60000,
+      },
+      url: [
+        process.env.CONNECT_TEST_ACCOUNT_URL,
+        `${process.env.CONNECT_TEST_ACCOUNT_URL}account/logins`,
+        `${process.env.CONNECT_TEST_ACCOUNT_URL}account/profile`,
+      ],
+    },
+    upload: {
+      target: "filesystem",
+      outputDir: "./lhci_reports",
+      reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.8 }],
+        "categories:accessibility": ["error", { minScore: 0.8 }],
+        "categories:best-practices": ["error", { minScore: 0.8 }],
+        "categories:seo": ["error", { minScore: 0.8 }],
+      },
+    },
+  },
+};
