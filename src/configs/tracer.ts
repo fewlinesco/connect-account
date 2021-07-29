66eae0af86a4e509148dc2c77b71fd07604665bb
Oluane <rochepauline6@gmail.com>
2021-07-29 10:55:02 +0200

    v0.10.3

--- src/configs/tracer.ts
+++ src/configs/tracer.ts
@@ -2,8 +2,6 @@ import { getTracer, startTracer, Tracer } from "@fwl/tracing";
 
 import { configVariables } from "@src/configs/config-variables";
 
-console.log(configVariables);
-
 startTracer({ collectors: configVariables.fwlTracingCollectors });
 
 function tracer(): Tracer {
