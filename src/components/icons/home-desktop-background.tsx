0bd0f5bfb972ad5a884b1e201b5ec4fd7c35d112
Oluane <rochepauline6@gmail.com>
2021-08-19 13:48:50 +0200

    migration home background

--- src/components/icons/home-desktop-background.tsx
+++ src/components/icons/home-desktop-background.tsx
@@ -1,229 +1,202 @@
 import React from "react";
-import styled from "styled-components";
-
-import { deviceBreakpoints } from "@src/design-system/theme";
 
 const HomeDesktopBackground: React.FC = () => {
   return (
-    <Wrapper>
-      <svg
-        width="744"
-        height="819"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        aria-hidden="true"
-      >
-        <circle cx="372" cy="411" r="371.5" stroke="#E5E5E5" />
-        <g clipPath="url(#clip0)">
-          <path fill="#fff" d="M23 159h80v80H23z" />
-          <path
-            d="M62.284 190.487c-5.04-17.653-19.16-13.338-29.009-3.389-28.844 29.138 47.608 45.722 53.18 28.723 5.57-16.999-19.132-7.682-24.17-25.334z"
-            fill="#DDDFF2"
-          />
-          <path
-            d="M95 192.2l-28.776-22.254a2 2 0 00-2.448 0L35 192.2h9.23V227h41.54v-27.187a2 2 0 00-2-2H60.076a2 2 0 00-2 2v15.084a2 2 0 002 2h9.846a2 2 0 002-2v-4.736"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
-        <g clipPath="url(#clip1)">
-          <path fill="#fff" d="M9 547h80v80H9z" />
-          <path
-            d="M38.486 568.655c-5.566-7.637-1.793-19.51 9.666-17.725 16.711.835 45.954 38.313 11.544 55.874-34.411 17.562-41.29-.663-29.107-7.15 12.183-6.486 13.463-23.363 7.897-30.999z"
-            fill="#DDDFF2"
-          />
-          <path
-            d="M53.392 585.968c0-9.004 7.3-16.304 16.304-16.304 9.005 0 16.304 7.3 16.304 16.304 0 9.004-7.996 16.304-17 16.304v6.114m-12.513-42.798H15.69c-1.105 0-2 .884-2 1.988v34.738c0 1.104.895 1.996 2 1.996h34.842L60 613v-11.747m-37.14-16.304h19.362m-19.361 10.19h25.475m-25.475-20.38h23.436"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <circle
-            cx="69"
-            cy="616"
-            r="1"
-            fill="#03083C"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
-        <g clipPath="url(#clip2)">
-          <path fill="#fff" d="M316 0h80v80h-80z" />
-          <path
-            d="M338.539 23.565c9.454 11.405 35.145-4.192 37.885 11.548-1.729 26.638-49.02 16.354-55.41 3.217-6.39-13.137 8.07-26.17 17.525-14.765z"
-            fill="#DDDFF2"
-          />
-          <path
-            d="M363 62h17.657c2.933 0 5.343-2.389 5.343-5.296V13.296C386 10.39 383.59 8 380.657 8h-33.419C344.305 8 342 10.389 342 13.296V18M356 18h24M360 28h20M360 38h20"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeMiterlimit="10"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M352 22a4 4 0 100-8 4 4 0 000 8zM370 51l3 3 5-6M341.844 44c-6.766 0-12.283 6.504-13.844 15.315 6.662 7.972 16.342 13.426 27.167 14.685.521-2.622.833-5.874.833-10.07C356 52.916 349.651 44 341.844 44z"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeMiterlimit="10"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <circle
-            cx="342"
-            cy="32"
-            r="7"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
-        <path fill="#fff" d="M652 548h80v80h-80z" />
+    <svg
+      width="744"
+      height="819"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      className="hidden lg:block fixed right-1/2 top-1/2 transform translate-x-1/2 -translate-y-1/2"
+    >
+      <circle cx="372" cy="411" r="371.5" stroke="#E5E5E5" />
+      <g clipPath="url(#clip0)">
+        <path fill="#fff" d="M23 159h80v80H23z" />
         <path
-          d="M685.225 566.669c-21.242 4.479-29.068 29.117-23.478 43.675 7.826 20.158 24.596-4.479 39.13-16.798 14.534-12.319 5.59-31.357-15.652-26.877z"
+          d="M62.284 190.487c-5.04-17.653-19.16-13.338-29.009-3.389-28.844 29.138 47.608 45.722 53.18 28.723 5.57-16.999-19.132-7.682-24.17-25.334z"
           fill="#DDDFF2"
         />
         <path
-          d="M664.642 568.614c7.81 0 20.826-7.646 26.358-11.47 5.532 3.824 18.548 11.47 26.358 11.47 0 40.528-17.572 50.66-26.358 50.66-8.786 0-26.358-10.132-26.358-50.66z"
+          d="M95 192.2l-28.776-22.254a2 2 0 00-2.448 0L35 192.2h9.23V227h41.54v-27.187a2 2 0 00-2-2H60.076a2 2 0 00-2 2v15.084a2 2 0 002 2h9.846a2 2 0 002-2v-4.736"
           stroke="#03083C"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
-        <rect
-          x="679"
-          y="583"
-          width="24"
-          height="20"
-          rx="1"
+      </g>
+      <g clipPath="url(#clip1)">
+        <path fill="#fff" d="M9 547h80v80H9z" />
+        <path
+          d="M38.486 568.655c-5.566-7.637-1.793-19.51 9.666-17.725 16.711.835 45.954 38.313 11.544 55.874-34.411 17.562-41.29-.663-29.107-7.15 12.183-6.486 13.463-23.363 7.897-30.999z"
+          fill="#DDDFF2"
+        />
+        <path
+          d="M53.392 585.968c0-9.004 7.3-16.304 16.304-16.304 9.005 0 16.304 7.3 16.304 16.304 0 9.004-7.996 16.304-17 16.304v6.114m-12.513-42.798H15.69c-1.105 0-2 .884-2 1.988v34.738c0 1.104.895 1.996 2 1.996h34.842L60 613v-11.747m-37.14-16.304h19.362m-19.361 10.19h25.475m-25.475-20.38h23.436"
           stroke="#03083C"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <circle
-          cx="691"
-          cy="591"
-          r="2"
+          cx="69"
+          cy="616"
+          r="1"
+          fill="#03083C"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <g clipPath="url(#clip2)">
+        <path fill="#fff" d="M316 0h80v80h-80z" />
+        <path
+          d="M338.539 23.565c9.454 11.405 35.145-4.192 37.885 11.548-1.729 26.638-49.02 16.354-55.41 3.217-6.39-13.137 8.07-26.17 17.525-14.765z"
+          fill="#DDDFF2"
+        />
+        <path
+          d="M363 62h17.657c2.933 0 5.343-2.389 5.343-5.296V13.296C386 10.39 383.59 8 380.657 8h-33.419C344.305 8 342 10.389 342 13.296V18M356 18h24M360 28h20M360 38h20"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M352 22a4 4 0 100-8 4 4 0 000 8zM370 51l3 3 5-6M341.844 44c-6.766 0-12.283 6.504-13.844 15.315 6.662 7.972 16.342 13.426 27.167 14.685.521-2.622.833-5.874.833-10.07C356 52.916 349.651 44 341.844 44z"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <circle
+          cx="342"
+          cy="32"
+          r="7"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <path fill="#fff" d="M652 548h80v80h-80z" />
+      <path
+        d="M685.225 566.669c-21.242 4.479-29.068 29.117-23.478 43.675 7.826 20.158 24.596-4.479 39.13-16.798 14.534-12.319 5.59-31.357-15.652-26.877z"
+        fill="#DDDFF2"
+      />
+      <path
+        d="M664.642 568.614c7.81 0 20.826-7.646 26.358-11.47 5.532 3.824 18.548 11.47 26.358 11.47 0 40.528-17.572 50.66-26.358 50.66-8.786 0-26.358-10.132-26.358-50.66z"
+        stroke="#03083C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <rect
+        x="679"
+        y="583"
+        width="24"
+        height="20"
+        rx="1"
+        stroke="#03083C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="691"
+        cy="591"
+        r="2"
+        stroke="#03083C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M691 593v5M688 583v-6c0-1 .6-3 3-3s3 2 3 3v6"
+        stroke="#03083C"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <g clipPath="url(#clip3)">
+        <path fill="#fff" d="M320 739h80v80h-80z" />
+        <path
+          d="M332.623 756.255c20.077-3.081 34.162 24.071 43.107 17.492 4.799-5.198 14.391-2.326 10.479 7.315-3.912 9.641-20.082 16.35-35.064 11.292-14.981-5.059-38.6-33.018-18.522-36.099z"
+          fill="#DDDFF2"
+        />
+        <circle
+          cx="359.81"
+          cy="763.527"
+          r="8.882"
+          transform="rotate(-135 359.81 763.527)"
           stroke="#03083C"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
         <path
-          d="M691 593v5M688 583v-6c0-1 .6-3 3-3s3 2 3 3v6"
+          d="M352.822 769.28l-6.988 6.987v16.52m20.963-23.507l6.988 6.987v16.52"
           stroke="#03083C"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
-        <g clipPath="url(#clip3)">
-          <path fill="#fff" d="M320 739h80v80h-80z" />
-          <path
-            d="M332.623 756.255c20.077-3.081 34.162 24.071 43.107 17.492 4.799-5.198 14.391-2.326 10.479 7.315-3.912 9.641-20.082 16.35-35.064 11.292-14.981-5.059-38.6-33.018-18.522-36.099z"
-            fill="#DDDFF2"
-          />
-          <circle
-            cx="359.81"
-            cy="763.527"
-            r="8.882"
-            transform="rotate(-135 359.81 763.527)"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M352.822 769.28l-6.988 6.987v16.52m20.963-23.507l6.988 6.987v16.52"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M340.449 780.434H329v25.941h24.706V780.434h-2.41M379.978 780.434H392v25.941h-25.941V780.434h2.531"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M366.623 757.493a1 1 0 101.417 1.411l-1.417-1.411zm10.027-7.236a1 1 0 00-1.418-1.411l1.418 1.411zm-24.926 8.647a1.001 1.001 0 001.418-1.411l-1.418 1.411zm-7.192-10.058a.999.999 0 10-1.417 1.411l1.417-1.411zm27.104 5.029l.709.705-.709-.705zm-18.494 3.618l-8.61-8.647-1.417 1.411 8.609 8.647 1.418-1.411zm14.898 1.411l4.305-4.324-1.417-1.411-4.305 4.324 1.417 1.411zm4.305-4.324l4.305-4.323-1.418-1.411-4.304 4.323 1.417 1.411z"
-            fill="#03083C"
-          />
-        </g>
-        <g clipPath="url(#clip4)">
-          <path fill="#fff" d="M644 168h80v80h-80z" />
-          <path
-            d="M717.327 185.446c-9.42-5.073-24.074 6.088-35.588 1.015-18.84-8.117-20.934 22.322 10.467 25.366 31.402 3.044 34.542-21.308 25.121-26.381z"
-            fill="#DDDFF2"
-          />
-          <path
-            d="M664.245 215.398l.444 6.882-5.226 1.401 1.401 5.226-9.624 3.278-1.973-9.974 11.348-19.655c-2.739-4.358-3.034-10.049-.281-14.816 3.961-6.862 12.735-9.213 19.598-5.251 6.862 3.962 9.213 12.736 5.252 19.598-2.753 4.767-7.829 7.357-12.973 7.165M707.083 212.619l6.166 3.066-1.384 5.216 5.21 1.41-1.944 9.957-9.595-3.295-11.347-19.655a14.36 14.36 0 01-7.216-1.66m18.754-5.002a14.35 14.35 0 00.248-14.797c-3.766-6.523-11.855-8.984-18.51-5.825"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <circle
-            cx="674"
-            cy="192"
-            r="3"
-            stroke="#03083C"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </g>
-        <defs>
-          <clipPath id="clip0">
-            <path fill="#fff" transform="translate(23 159)" d="M0 0h80v80H0z" />
-          </clipPath>
-          <clipPath id="clip1">
-            <path fill="#fff" transform="translate(9 547)" d="M0 0h80v80H0z" />
-          </clipPath>
-          <clipPath id="clip2">
-            <path fill="#fff" transform="translate(316)" d="M0 0h80v80H0z" />
-          </clipPath>
-          <clipPath id="clip3">
-            <path
-              fill="#fff"
-              transform="translate(320 739)"
-              d="M0 0h80v80H0z"
-            />
-          </clipPath>
-          <clipPath id="clip4">
-            <path
-              fill="#fff"
-              transform="translate(644 168)"
-              d="M0 0h80v80H0z"
-            />
-          </clipPath>
-        </defs>
-      </svg>
-    </Wrapper>
+        <path
+          d="M340.449 780.434H329v25.941h24.706V780.434h-2.41M379.978 780.434H392v25.941h-25.941V780.434h2.531"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M366.623 757.493a1 1 0 101.417 1.411l-1.417-1.411zm10.027-7.236a1 1 0 00-1.418-1.411l1.418 1.411zm-24.926 8.647a1.001 1.001 0 001.418-1.411l-1.418 1.411zm-7.192-10.058a.999.999 0 10-1.417 1.411l1.417-1.411zm27.104 5.029l.709.705-.709-.705zm-18.494 3.618l-8.61-8.647-1.417 1.411 8.609 8.647 1.418-1.411zm14.898 1.411l4.305-4.324-1.417-1.411-4.305 4.324 1.417 1.411zm4.305-4.324l4.305-4.323-1.418-1.411-4.304 4.323 1.417 1.411z"
+          fill="#03083C"
+        />
+      </g>
+      <g clipPath="url(#clip4)">
+        <path fill="#fff" d="M644 168h80v80h-80z" />
+        <path
+          d="M717.327 185.446c-9.42-5.073-24.074 6.088-35.588 1.015-18.84-8.117-20.934 22.322 10.467 25.366 31.402 3.044 34.542-21.308 25.121-26.381z"
+          fill="#DDDFF2"
+        />
+        <path
+          d="M664.245 215.398l.444 6.882-5.226 1.401 1.401 5.226-9.624 3.278-1.973-9.974 11.348-19.655c-2.739-4.358-3.034-10.049-.281-14.816 3.961-6.862 12.735-9.213 19.598-5.251 6.862 3.962 9.213 12.736 5.252 19.598-2.753 4.767-7.829 7.357-12.973 7.165M707.083 212.619l6.166 3.066-1.384 5.216 5.21 1.41-1.944 9.957-9.595-3.295-11.347-19.655a14.36 14.36 0 01-7.216-1.66m18.754-5.002a14.35 14.35 0 00.248-14.797c-3.766-6.523-11.855-8.984-18.51-5.825"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <circle
+          cx="674"
+          cy="192"
+          r="3"
+          stroke="#03083C"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0">
+          <path fill="#fff" transform="translate(23 159)" d="M0 0h80v80H0z" />
+        </clipPath>
+        <clipPath id="clip1">
+          <path fill="#fff" transform="translate(9 547)" d="M0 0h80v80H0z" />
+        </clipPath>
+        <clipPath id="clip2">
+          <path fill="#fff" transform="translate(316)" d="M0 0h80v80H0z" />
+        </clipPath>
+        <clipPath id="clip3">
+          <path fill="#fff" transform="translate(320 739)" d="M0 0h80v80H0z" />
+        </clipPath>
+        <clipPath id="clip4">
+          <path fill="#fff" transform="translate(644 168)" d="M0 0h80v80H0z" />
+        </clipPath>
+      </defs>
+    </svg>
   );
 };
-
-const Wrapper = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  @media ${deviceBreakpoints.m} {
-    display: none;
-  }
-`;
 
 export { HomeDesktopBackground };
