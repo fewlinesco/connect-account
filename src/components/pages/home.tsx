1007f28afe5dae812019a96efeac80788fc3ed55
Oluane <rochepauline6@gmail.com>
2021-08-19 13:44:06 +0200

    home compo migration

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { useIntl } from "react-intl";
-import styled from "styled-components";
 
+import { SectionBox } from "@src/components/boxes";
 import { ButtonVariant } from "@src/components/buttons/buttons";
 import { FakeButton } from "@src/components/buttons/fake-button";
 import { HomeDesktopBackground } from "@src/components/icons/home-desktop-background";
 import { HomeMobileBackground } from "@src/components/icons/home-mobile-background";
 import { NeutralLink } from "@src/components/neutral-link";
-import { SectionBox } from "@src/components/section-box";
-import { deviceBreakpoints } from "@src/design-system/theme";
 
 const Home: React.FC<{ authorizeURL: string; providerName: string }> = ({
   authorizeURL,
@@ -20,66 +18,18 @@ const Home: React.FC<{ authorizeURL: string; providerName: string }> = ({
     <>
       <HomeDesktopBackground />
       <HomeMobileBackground />
-      <Wrapper>
-        <Container>
-          <SectionBox>
-            <Flex>
-              <DescriptionText>
-                {formatMessage({ id: "info" })} {providerName}
-              </DescriptionText>
-              <NeutralLink href={authorizeURL}>
-                <FakeButton variant={ButtonVariant.PRIMARY}>
-                  {formatMessage({ id: "callToAction" })}
-                </FakeButton>
-              </NeutralLink>
-            </Flex>
-          </SectionBox>
-        </Container>
-      </Wrapper>
+      <SectionBox className="w-4/5 lg:max-w-2xl flex flex-col justify-center p-16 absolute right-1/2 top-28 lg:top-1/2 transform translate-x-1/2 lg:-translate-y-1/2">
+        <p className="text-center mb-8 font-semibold">
+          {formatMessage({ id: "info" })} {providerName}
+        </p>
+        <NeutralLink href={authorizeURL}>
+          <FakeButton variant={ButtonVariant.PRIMARY}>
+            {formatMessage({ id: "callToAction" })}
+          </FakeButton>
+        </NeutralLink>
+      </SectionBox>
     </>
   );
 };
-
-const Wrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  @media ${deviceBreakpoints.m} {
-    top: 7rem;
-    align-items: flex-start;
-    height: auto;
-  }
-`;
-
-const Container = styled.div`
-  max-width: 40rem;
-
-  @media ${deviceBreakpoints.m} {
-    width: 80%;
-  }
-`;
-
-const Flex = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: ${({ theme }) => theme.spaces.s};
-
-  div {
-    margin: 0;
-  }
-`;
-
-const DescriptionText = styled.p`
-  text-align: center;
-  margin: 0 0 ${({ theme }) => theme.spaces.xs} 0;
-  font-weight: ${({ theme }) => theme.fontWeights.semibold};
-`;
 
 export { Home };
