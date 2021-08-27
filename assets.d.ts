863cc673b282e03f8bd1c1e810e137e7bead9d8a
Fuzznimp <montaigne.anthony.dev@gmail.com>
2021-08-27 13:37:14 +0200

    Removed asset type def

--- assets.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.png";
-declare module "*.svg";
-declare module "*.jpg";
-declare module "*.gif";
-declare module "*.eot";
-declare module "*.ttf";
-declare module "*.woff";
-declare module "*.woff2";
