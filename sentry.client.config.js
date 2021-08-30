60ac46b559e3335c39f2b12990102bb93945da9f
Oluane <rochepauline6@gmail.com>
2021-08-30 17:07:55 +0200

    wrapping the script inside a try catch block

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,33 +5,37 @@
 import { WebError } from "@fwl/web/dist/errors";
 import * as Sentry from "@sentry/nextjs";
 
-const sentryUserConsent = document.cookie
-  ? document.cookie
-      .split("; ")
-      .find((cookie) => cookie.startsWith("user-consent"))
-      .split("=")[1]
-  : undefined;
+try {
+  const sentryUserConsent = document.cookie
+    ? document.cookie
+        .split("; ")
+        .find((cookie) => cookie.startsWith("user-consent"))
+        .split("=")[1]
+    : undefined;
 
-const parsedSentryUserConsent = sentryUserConsent
-  ? JSON.parse(decodeURIComponent(sentryUserConsent)).sentry
-  : false;
+  const parsedSentryUserConsent = sentryUserConsent
+    ? JSON.parse(decodeURIComponent(sentryUserConsent)).sentry
+    : false;
 
-if (parsedSentryUserConsent) {
-  Sentry.init({
-    dsn: "https://6baf0213093a428d9e9ec7ae0979868c@o287463.ingest.sentry.io/5387583",
-    tracesSampleRate: 1.0,
-    environment:
-      process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || process.env.NODE_ENV,
-    enabled:
-      process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === "development"
-        ? false
-        : process.env.NODE_ENV === "production",
-    beforeSend(event, hint) {
-      if (hint.originalException instanceof WebError) {
-        return null;
-      }
+  if (parsedSentryUserConsent) {
+    Sentry.init({
+      dsn: "https://6baf0213093a428d9e9ec7ae0979868c@o287463.ingest.sentry.io/5387583",
+      tracesSampleRate: 1.0,
+      environment:
+        process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || process.env.NODE_ENV,
+      enabled:
+        process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT === "development"
+          ? false
+          : process.env.NODE_ENV === "production",
+      beforeSend(event, hint) {
+        if (hint.originalException instanceof WebError) {
+          return null;
+        }
 
-      return event;
-    },
-  });
+        return event;
+      },
+    });
+  }
+} catch (error) {
+  return;
 }
